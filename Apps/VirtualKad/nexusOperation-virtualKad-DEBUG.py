145723f52bd9084980cd7950068a9771317b8ca8
ISDADS\AAA34169 <fabien.leboeuf@gmail.com>
2016-10-07 22:36:43 +0100

    [new App] - virtual KAD

---
+++ Apps/VirtualKad/nexusOperation-virtualKad-DEBUG.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 03 10:40:17 2016
+
+@author: fabien Leboeuf ( Salford Univ)
+"""
+
+import pdb
+import logging
+
+try:
+    import pyCGM2.pyCGM2_CONFIG 
+except ImportError:
+    logging.error("[pyCGM2] : pyCGM2 module not in your python path")
+
+
+# vicon
+pyCGM2.pyCGM2_CONFIG.addNexusPythonSdk()
+import ViconNexus
+
+# openMA
+pyCGM2.pyCGM2_CONFIG.addOpenma()
+import ma.io
+import ma.body
+
+# local lib
+import lib.functions  as libf
+
+    
+
+
+    
+if __name__ == "__main__":
+    print "======== [pyCGM2-Virtual KAD] ========="
+    
+#    pyNEXUS = ViconNexus.ViconNexus()    
+#    NEXUS_PYTHON_CONNECTED = pyNEXUS.Client.IsConnected()
+    
+    NEXUS_PYTHON_CONNECTED = True
+ 
+    if NEXUS_PYTHON_CONNECTED: 
+
+        # ---- INPUTS ----
+        vertical_global_axis = "Z" 
+        left_medial_knee_marker = "LMEPI"
+        right_medial_knee_marker ="RMEPI"            
+        updateC3d = False
+
+        # ---- DATA ----
+        
+        DATA_PATH = "C:\\Users\\AAA34169\\Documents\\VICON DATA\\pyCGM2-Data\\CGM1\\CGM1-NexusPlugin\\virtualKad\\"
+        filename =  "MRI-US-01, 2008-08-08, 3DGA 02.c3d"
+
+        
+        # ---- PROCESSING ----
+        fullFilenameVirtualKad = libf.virtualKAD (filename,DATA_PATH, 
+                    vertical_global_axis = vertical_global_axis, 
+                    left_medial_knee_marker = left_medial_knee_marker, 
+                    right_medial_knee_marker = right_medial_knee_marker, 
+                    updateC3d = updateC3d)
+
+
+        
+        if updateC3d :
+            logging.warning ("[pyCGM2-virtual Kad] Static file updated")
+        else: 
+            logging.info ("[pyCGM2-virtual Kad] New static file created")   
+    else:
+        logging.error("[[pyCGM2-virtual Kad]] : Nexus Not Connected")
+        
+
+        
+         
---
+++ Apps/VirtualKad/nexusOperation-virtualKad.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 03 10:40:17 2016
+
+@author: fabien Leboeuf ( Salford Univ)
+"""
+
+import sys
+import pdb
+import logging
+
+try:
+    import pyCGM2.pyCGM2_CONFIG 
+except ImportError:
+    logging.error("[pyCGM2] : pyCGM2 module not in your python path")
+
+
+# vicon
+pyCGM2.pyCGM2_CONFIG.addNexusPythonSdk()
+import ViconNexus
+
+# openMA
+pyCGM2.pyCGM2_CONFIG.addOpenma()
+import ma.io
+import ma.body
+
+#local lib
+import lib.functions  as libf
+
+    
+
+
+    
+if __name__ == "__main__":
+    
+    print "======== [pyCGM2-Virtual KAD] ========="    
+    
+    pyNEXUS = ViconNexus.ViconNexus()    
+    NEXUS_PYTHON_CONNECTED = pyNEXUS.Client.IsConnected()
+    
+     
+    if NEXUS_PYTHON_CONNECTED: 
+
+        # ---- INPUTS ----
+        vertical_global_axis = sys.argv[1] #"Z" 
+        left_medial_knee_marker =  sys.argv[2] #"LMEPI"
+        right_medial_knee_marker =  sys.argv[3]# "RMEPI"            
+        updateC3d = bool(int(sys.argv[4])) #False
+
+        # ---- DATA ----
+        DATA_PATH, filenameNoExt = pyNEXUS.GetTrialName()
+        filename = filenameNoExt+".c3d"
+        print "Name: "+ filename
+        print "Path: "+ DATA_PATH
+        
+
+        
+        # ---- PROCESSING ----
+        fullFilenameVirtualKad = libf.virtualKAD (filename,DATA_PATH, 
+                    vertical_global_axis = vertical_global_axis, 
+                    left_medial_knee_marker = left_medial_knee_marker, 
+                    right_medial_knee_marker = right_medial_knee_marker, 
+                    updateC3d = updateC3d)
+
+    else:
+        logging.error("[[pyCGM2-virtual Kad]] : Nexus Not Connected")
+        
+
+        
+         
