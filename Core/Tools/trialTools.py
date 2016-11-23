2868798d2fd6291be222d00088466564849e7f2c
ISDADS\AAA34169 <fabien.leboeuf@gmail.com>
2016-11-23 11:25:11 +0000

    function findProgressionFromPoints compatible with openMA

--- Core/Tools/trialTools.py
+++ Core/Tools/trialTools.py
@@ -4,6 +4,8 @@ Created on Tue Oct 04 16:17:08 2016
 
 @author: fabien Leboeuf
 """
+import numpy as np
+import pdb
 
 # openMA
 import ma.io
@@ -78,8 +80,6 @@ def isKineticFlag(trial):
     else:
         return True,kineticEvent_times
 
-      
-            
             
 def automaticKineticDetection(dataPath,filenames):
     kineticTrials=[]
@@ -101,3 +101,59 @@ def automaticKineticDetection(dataPath,filenames):
     kineticTrials = None if kineticTrials ==[] else kineticTrials    
     
     return kineticTrials,kineticFilenames,flag_kinetics            
+
+
+def findProgressionFromPoints(trial,originPointLabel, longitudinal_extremityPointLabel,lateral_extremityPointLabel):
+    if not isTimeSequenceExist(trial,originPointLabel):
+        raise Exception( "[pyCGM2] : origin point doesnt exist")
+
+    if not isTimeSequenceExist(trial,longitudinal_extremityPointLabel):
+        raise Exception( "[pyCGM2] : longitudinal point  doesnt exist")
+
+    if not isTimeSequenceExist(trial,lateral_extremityPointLabel):
+        raise Exception( "[pyCGM2] : lateral point  doesnt exist")
+
+    originValues = trial.findChild(ma.T_TimeSequence,originPointLabel).data()[0,0:3]
+    longitudinal_extremityValues = trial.findChild(ma.T_TimeSequence,longitudinal_extremityPointLabel).data()[0,0:3]
+    lateral_extremityValues = trial.findChild(ma.T_TimeSequence,lateral_extremityPointLabel).data()[0,0:3]#[ acq.GetPoint(originPointLabel).GetValues()[0,:]
+
+
+    a1=(longitudinal_extremityValues-originValues)
+    a1=a1/np.linalg.norm(a1)
+
+    a2=(lateral_extremityValues-originValues)
+    a2=a2/np.linalg.norm(a2)
+
+    globalAxes = {"X" : np.array([1,0,0]), "Y" : np.array([0,1,0]), "Z" : np.array([0,0,1])}
+
+    # longitudinal axis    
+    tmp=[]
+    for axis in globalAxes.keys():
+        res = np.dot(a1,globalAxes[axis])
+        tmp.append(res)
+    maxIndex = np.argmax(np.abs(tmp))
+    longitudinalAxis =  globalAxes.keys()[maxIndex]
+    forwardProgression = True if tmp[maxIndex]>0 else False
+    
+    # lateral axis
+    tmp=[]
+    for axis in globalAxes.keys():
+        res = np.dot(a2,globalAxes[axis])
+        tmp.append(res)
+    maxIndex = np.argmax(np.abs(tmp))
+    lateralAxis =  globalAxes.keys()[maxIndex]    
+    
+
+    # global frame
+    if "X" not in str(longitudinalAxis+lateralAxis):
+        globalFrame = str(longitudinalAxis+lateralAxis+"X")
+    if "Y" not in str(longitudinalAxis+lateralAxis):
+        globalFrame = str(longitudinalAxis+lateralAxis+"Y")        
+    if "Z" not in str(longitudinalAxis+lateralAxis):
+        globalFrame = str(longitudinalAxis+lateralAxis+"Z")        
+
+    logging.info("Longitudinal axis : %s"%(longitudinalAxis))
+    logging.info("forwardProgression : %s"%(str(forwardProgression)))
+    logging.info("globalFrame : %s"%(str(globalFrame)))
+
+    return   longitudinalAxis,forwardProgression,globalFrame
