ccec8566a53a28e2a83505de979af2af39ccb379
AAA34169 <fabien.leboeuf@gmail.com>
2017-02-21 15:26:32 +0000

    comment ViconInterface

--- pyCGM2/viconInterface.py
+++ pyCGM2/viconInterface.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import btk
-
-
 class ViconInterface(object):
-
     def __init__(self,NEXUS,iModel, iAcq, vskName, staticProcessing = False ):
+        """
+            Constructor
+           
+            :Parameters:
+                - `NEXUS` () - Nexus environment 
+                - `iModel` (pyCGM2.Model.CGM2.Model) - model instance
+                - `vskName` (str) . subject name create in Nexus
+                - `staticProcessingFlag` (bool`) : flag indicating only static model ouput will be export  
+                
+        """
         self.m_model = iModel
         self.m_acq = iAcq
         self.m_vskName = vskName
@@ -13,6 +19,9 @@ class ViconInterface(object):
         self.staticProcessing = staticProcessing
 
     def run(self):
+        """
+            method calling embedded-model method : viconExport  
+        """
         self.m_model.viconExport(self.NEXUS,self.m_acq, self.m_vskName,self.staticProcessing)
         
         
