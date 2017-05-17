a9be894096106b76bd766151e5c4ac660db96f86
AAA34169 <fabien.leboeuf@gmail.com>
2017-05-17 14:18:02 +0100

    new module eclipse -  include a function altered FP in enf file

---
+++ pyCGM2/eclipse.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# pyCGM2 settings
+import logging
+import os
+
+import pyCGM2
+pyCGM2.CONFIG.setLoggingLevel(logging.INFO)
+
+from pyCGM2.Model.CGM2 import forceplates
+from pyCGM2.Tools import btkTools
+
+import configparser
+
+
+def enfForcePlateAssignment(c3dFullFilename):
+    """
+        Add Force plate assignement in the enf file 
+
+        :Parameters:
+            - `c3dFullFilename` (str) - filename with path of the c3d 
+    """
+        
+    acqGait = btkTools.smartReader(str(c3dFullFilename))
+    enfFile = str(c3dFullFilename[:-4]+".Trial.enf")
+
+    if not os.path.isfile(enfFile):
+        raise Exception ("[pyCGM2] - No enf file associated with the c3d")
+    else:            
+
+        # detect Force plate    
+        mappedForcePlate = forceplates.matchingFootSideOnForceplate(acqGait)
+        logging.info("Force plate assignment : %s" %mappedForcePlate)
+    
+        # --------------------Modify ENF --------------------------------------
+        configEnf = configparser.ConfigParser()
+        configEnf.optionxform = str
+        configEnf.read(enfFile)
+        
+        
+        indexFP=1
+        for letter in mappedForcePlate:
+            
+            if letter =="L": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Left"  
+            if letter =="R": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Right"  
+            if letter =="X": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Invalid"  
+         
+            indexFP+=1
+        
+        tmpFile =str(c3dFullFilename[:-4]+".Trial.enf-tmp")
+        with open(tmpFile, 'w') as configfile:
+            configEnf.write(configfile)
+        
+        os.remove(enfFile)
+        os.rename(tmpFile,enfFile)
+        logging.warning("Enf file updated with Force plate assignment") 
