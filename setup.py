56e61b7c55444cd1a8cc64e23a95339be4a70ae3
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-05-25 10:55:34 +0100

    setup.py modification so to use the command "python setup.py install" and copy all folder out of source

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup
+from setuptools import setup,find_packages
+import os
+
+def gen_data_files(*dirs):
+    results = []
+
+    for src_dir in dirs:
+        for root,dirs,files in os.walk(src_dir):
+            results.append((root, map(lambda f:root + "/" + f, files)))
+    return results
+
 setup(name = 'pyCGM2',
     version = '0.0.1',
     author = 'fabien Leboeuf',
     author_email = 'fabien.leboeuf@gmail.com',
     keywords = 'python Conventional Gait Model',
-    packages = ['pyCGM2'],
-    install_requires = ['numpy>=1.11.0', 
+    packages=find_packages(),
+	data_files = gen_data_files("Apps","Data","Extern","NoViconApps","Session Settings","third party"),
+	include_package_data=True,
+	install_requires = ['numpy>=1.11.0', 
                         'scipy>=0.17.0',
                         'matplotlib>=1.5.3',
                         'pandas >=0.19.1',
