e024af3c7736a60a926ad9a12dfdac014c9ff70e
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-07-13 13:02:32 +0100

    Method Sara Checked

--- Apps/Knee Calibration/nexusOperation-pyCGM2-SARA.py
+++ Apps/Knee Calibration/nexusOperation-pyCGM2-SARA.py
@@ -17,8 +17,8 @@ pyCGM2.CONFIG.setLoggingLevel(logging.INFO)
 
 # vicon nexus
 import ViconNexus
-    
-# pyCGM2 libraries    
+
+# pyCGM2 libraries
 from pyCGM2.Tools import btkTools,nexusTools
 from pyCGM2.Model.CGM2 import cgm2, modelFilters, modelDecorator
 import pyCGM2.enums as pyCGM2Enums
@@ -30,7 +30,7 @@ from pyCGM2 import viconInterface
 def detectSide(acq,left_markerLabel,right_markerLabel):
 
     flag,vff,vlf = btkTools.findValidFrames(acq,[left_markerLabel,right_markerLabel])
-    
+
     left = acq.GetPoint(left_markerLabel).GetValues()[vff:vlf,2]
     right = acq.GetPoint(right_markerLabel).GetValues()[vff:vlf,2]
 
@@ -38,12 +38,13 @@ def detectSide(acq,left_markerLabel,right_markerLabel):
 
     return side
 
-    
-if __name__ == "__main__":
-   
-    plt.close("all")
-    DEBUG = True
 
+if __name__ == "__main__":
+
+    plt.close("all")
+    DEBUG = False
+
+    pdb.set_trace()
     parser = argparse.ArgumentParser(description='SARA Functional Knee Calibration')
     parser.add_argument('-s','--side', type=str, help="Side : Left or Right")
     parser.add_argument('-b','--beginFrame', type=int, help="begin frame")
@@ -59,7 +60,7 @@ if __name__ == "__main__":
         # --------------------------PATH + FILE ------------------------------------
 
         if DEBUG:
-            DATA_PATH = pyCGM2.CONFIG.TEST_DATA_PATH + "CGM2\\knee calibration\\CGM2.3-calibrationSara\\" 
+            DATA_PATH = pyCGM2.CONFIG.TEST_DATA_PATH + "CGM2\\knee calibration\\CGM2.3-calibrationSara\\"
             reconstructedFilenameLabelledNoExt = "Right Knee"
             NEXUS.OpenTrial( str(DATA_PATH+reconstructedFilenameLabelledNoExt), 30 )
 
@@ -92,11 +93,10 @@ if __name__ == "__main__":
         logging.info("loaded model : %s" %(model.version ))
 
         # --------------------------CONFIG ------------------------------------
-        
-            
-        
 
-        # --------------------CHECKING ------------------------------       
+
+
+        # --------------------CHECKING ------------------------------
         if model.version in ["CGM1.0","CGM1.1","CGM2.1","CGM2.2","CGM2.2e"] :
             raise Exception ("Can t use SARA method with your model %s [minimal version : CGM2.3]"%(model.version))
         elif model.version == "CGM2.3":
@@ -109,38 +109,38 @@ if __name__ == "__main__":
                inputs = json.loads(open(str(pyCGM2.CONFIG.PYCGM2_APPDATA_PATH+"CGM2_4-Expert-pyCGM2.settings")).read(),object_pairs_hook=OrderedDict)
         else:
             raise Exception ("model version not found [contact admin]")
-        
+
         # --------------------------SESSION INFOS ------------------------------------
         # info file
         infoSettings = fileManagement.manage_pycgm2SessionInfos(DATA_PATH,subject)
-        
+
         #  translators management
         if model.version in  ["CGM2.3","CGM2.3e"]:
             translators = fileManagement.manage_pycgm2Translators(DATA_PATH,"CGM2-3.translators")
         elif model.version in  ["CGM2.4","CGM2.4e"]:
             translators = fileManagement.manage_pycgm2Translators(DATA_PATH,"CGM2-4.translators")
         if not translators:
-           translators = inputs["Translators"]   
-            
+           translators = inputs["Translators"]
+
         # --------------------------ACQ WITH TRANSLATORS --------------------------------------
 
         # --- btk acquisition ----
         acqFunc = btkTools.smartReader(str(DATA_PATH + reconstructFilenameLabelled))
         btkTools.checkMultipleSubject(acqFunc)
         acqFunc =  btkTools.applyTranslators(acqFunc,translators)
-        
+
         #---get frame range of interest---
         ff = acqFunc.GetFirstFrame()
         lf = acqFunc.GetLastFrame()
-        
+
         initFrame = args.beginFrame if args.beginFrame is not None else ff
-        endFrame = args.endFrame if args.endFrame is not None else lf    
-                
+        endFrame = args.endFrame if args.endFrame is not None else lf
+
         iff=initFrame-ff
         ilf=endFrame-ff
-        
-        
-        #---motion side of the lower limb--- 
+
+
+        #---motion side of the lower limb---
         if args.side is None:
             side = detectSide(acqFunc,"LANK","RANK")
             logging.info("Detected motion side : %s" %(side) )
@@ -148,7 +148,7 @@ if __name__ == "__main__":
             side = args.side
 
         # --------------------------RESET OF THE STATIC File---------
- 
+
         # load btkAcq from static file
         staticFilename = model.m_staticFilename
         acqStatic = btkTools.smartReader(str(DATA_PATH+staticFilename))
@@ -161,30 +161,30 @@ if __name__ == "__main__":
         flag_rightFlatFoot = initialCalibration["rightFlatFoot"]
         markerDiameter = initialCalibration["markerDiameter"]
 
-        if side == "Left":  
+        if side == "Left":
             # remove other functional calibration
             model.mp_computed["LeftKnee2DofOffset"] = 0
-            
+
             # reinit node and func offset of the left side from initial calibration
             useLeftHJCnodeLabel = initialCalibration["LHJC_node"]
             useLeftKJCnodeLabel = initialCalibration["LKJC_node"]
             useLeftAJCnodeLabel = initialCalibration["LAJC_node"]
-            model.mp_computed["LeftKneeFuncCalibrationOffset"] = 0 
+            model.mp_computed["LeftKneeFuncCalibrationOffset"] = 0
             model.mp_computed["FinalFuncLeftThighRotationOffset"] =0
-            
+
 
             # opposite side - keep node from former calibration
             if model.mp_computed["RightKneeFuncCalibrationOffset"]:
                 useRightHJCnodeLabel = model.m_properties["CalibrationParameters"]["RHJC_node"]
                 useRightKJCnodeLabel = model.m_properties["CalibrationParameters"]["RKJC_node"]
                 useRightAJCnodeLabel = model.m_properties["CalibrationParameters"]["RAJC_node"]
-                
+
             else:
                 useRightHJCnodeLabel = initialCalibration["RHJC_node"]
                 useRightKJCnodeLabel = initialCalibration["RKJC_node"]
                 useRightAJCnodeLabel = initialCalibration["RAJC_node"]
 
-        if side == "Right":  
+        if side == "Right":
             # remove other functional calibration
             model.mp_computed["RightKnee2DofOffset"] = 0
 
@@ -194,19 +194,19 @@ if __name__ == "__main__":
             useRightAJCnodeLabel = initialCalibration["RAJC_node"]
             model.mp_computed["RightKneeFuncCalibrationOffset"] = 0
             model.mp_computed["FinalFuncRightThighRotationOffset"] =0
-            
+
             # opposite side - keep node from former calibration
             if model.mp_computed["LeftKneeFuncCalibrationOffset"]:
                 useLeftHJCnodeLabel = model.m_properties["CalibrationParameters"]["LHJC_node"]
                 useLeftKJCnodeLabel = model.m_properties["CalibrationParameters"]["LKJC_node"]
                 useLeftAJCnodeLabel = model.m_properties["CalibrationParameters"]["LAJC_node"]
-                
+
             else:
                 useLeftHJCnodeLabel = initialCalibration["LHJC_node"]
                 useLeftKJCnodeLabel = initialCalibration["LKJC_node"]
                 useLeftAJCnodeLabel = initialCalibration["LAJC_node"]
 
-        
+
         # ---- Reset Calibration
         logging.debug("SARA --%s --- first Calibration"%(side) )
         logging.debug(" node (LHJC) => %s" %(useLeftHJCnodeLabel))
@@ -220,7 +220,7 @@ if __name__ == "__main__":
 
 
         # no rotation of both thigh
-        scp=modelFilters.StaticCalibrationProcedure(model) 
+        scp=modelFilters.StaticCalibrationProcedure(model)
         modelFilters.ModelCalibrationFilter(scp,acqStatic,model,
                                useLeftHJCnode=useLeftHJCnodeLabel, useRightHJCnode=useRightHJCnodeLabel,
                                useLeftKJCnode=useLeftKJCnodeLabel, useLeftAJCnode=useLeftAJCnodeLabel,
@@ -228,30 +228,30 @@ if __name__ == "__main__":
                                leftFlatFoot = flag_leftFlatFoot, rightFlatFoot = flag_rightFlatFoot,
                                markerDiameter=markerDiameter,
                                RotateLeftThighFlag = False,
-                               RotateRightThighFlag = False).compute()        
-        
+                               RotateRightThighFlag = False).compute()
 
 
-        btkTools.smartWriter(acqStatic, "acqStatic0-test.c3d")
-        
+
+        #btkTools.smartWriter(acqStatic, "acqStatic0-test.c3d")
+
         if model.version in  ["CGM2.3","CGM2.3e","CGM2.3","CGM2.4e"]:
             if side == "Left":
                 thigh_markers = model.getSegment("Left Thigh").m_tracking_markers
                 shank_markers = model.getSegment("Left Shank").m_tracking_markers
- 
+
             elif side == "Right":
                 thigh_markers = model.getSegment("Right Thigh").m_tracking_markers
                 shank_markers = model.getSegment("Right Shank").m_tracking_markers
 
-            validFrames,vff,vlf = btkTools.findValidFrames(acqFunc,thigh_markers+shank_markers) 
+            validFrames,vff,vlf = btkTools.findValidFrames(acqFunc,thigh_markers+shank_markers)
 
             proximalSegmentLabel=str(side+" Thigh")
-            distalSegmentLabel=str(side+" Shank")        
-              
-            # segment Motion  
+            distalSegmentLabel=str(side+" Shank")
+
+            # segment Motion
             modMotion=modelFilters.ModelMotionFilter(scp,acqFunc,model,pyCGM2Enums.motionMethod.Sodervisk)
             modMotion.segmentalCompute([proximalSegmentLabel,distalSegmentLabel])
-        
+
             # decorator
             modelDecorator.KneeCalibrationDecorator(model).sara(side,
                                                                 indexFirstFrame = iff,
@@ -259,24 +259,24 @@ if __name__ == "__main__":
 
 
             # --------------------------FINAL CALIBRATION OF THE STATIC File---------
-               
+
             if side == "Left":
                 useLeftHJCnodeLabel = model.m_properties["CalibrationParameters"]["LHJC_node"]
                 useLeftKJCnodeLabel = "KJC_Sara"
                 useLeftAJCnodeLabel = model.m_properties["CalibrationParameters"]["LAJC_node"]
                 useRotateLeftThighFlag = True
                 useRotateRightThighFlag = False
-                
+
             elif side == "Right":
                 useRightHJCnodeLabel = model.m_properties["CalibrationParameters"]["RHJC_node"]
                 useRightKJCnodeLabel = "KJC_Sara"
                 useRightAJCnodeLabel = model.m_properties["CalibrationParameters"]["RAJC_node"]
                 useRotateRightThighFlag = True
-                useRotateLeftThighFlag = False 
+                useRotateLeftThighFlag = False
 
-                
+
             # ----  Calibration
-            
+
             logging.debug("SARA --%s --- final Calibration"%(side) )
             logging.debug(" node (LHJC) => %s" %(useLeftHJCnodeLabel))
             logging.debug(" node (LKJC) => %s" %(useLeftKJCnodeLabel))
@@ -288,7 +288,7 @@ if __name__ == "__main__":
             logging.debug(" node (RAJC) => %s" %(useRightAJCnodeLabel))
             logging.debug(" rotated Right Thigh => %s" %(str(useRotateRightThighFlag)))
 
-            
+
             modelFilters.ModelCalibrationFilter(scp,acqStatic,model,
                                useLeftHJCnode=useLeftHJCnodeLabel, useRightHJCnode=useRightHJCnodeLabel,
                                useLeftKJCnode=useLeftKJCnodeLabel, useLeftAJCnode=useLeftAJCnodeLabel,
@@ -297,23 +297,28 @@ if __name__ == "__main__":
                                markerDiameter=markerDiameter,
                                RotateLeftThighFlag = useRotateLeftThighFlag,
                                RotateRightThighFlag = useRotateRightThighFlag).compute()
-                
-            
+
+
+            #btkTools.smartWriter(acqStatic, "acqStatic1-test.c3d")
+
             # ----------------------SAVE-------------------------------------------
             if os.path.isfile(DATA_PATH + subject + "-pyCGM2.model"):
                 logging.warning("previous model removed")
                 os.remove(DATA_PATH + subject + "-pyCGM2.model")
-    
+
             modelFile = open(DATA_PATH + subject+"-pyCGM2.model", "w")
             cPickle.dump(model, modelFile)
             modelFile.close()
             logging.warning("model updated with a  %s knee calibrated with SARA method" %(side))
-            
-            
+
+
 
             # ----------------------VICON INTERFACE-------------------------------------------
             #--- update mp
             viconInterface.updateNexusSubjectMp(NEXUS,model,subject)
+
+
+            #---points from first motio filter
 
             # -- add nexus Bones
             if side == "Left":
@@ -324,23 +329,31 @@ if __name__ == "__main__":
             # add modelled markers
             meanOr_inThigh = model.getSegment(proximalSegmentLabel).getReferential("TF").getNodeTrajectory("KJC_Sara")
             meanAxis_inThigh = model.getSegment(proximalSegmentLabel).getReferential("TF").getNodeTrajectory("KJC_SaraAxis")
-
             btkTools.smartAppendPoint(acqFunc,side+"_KJC_Sara",meanOr_inThigh)
-            btkTools.smartAppendPoint(acqFunc,side+"_KJC_SaraAxis",meanAxis_inThigh) 
+            btkTools.smartAppendPoint(acqFunc,side+"_KJC_SaraAxis",meanAxis_inThigh)
 
             nexusTools.appendModelledMarkerFromAcq(NEXUS,subject,side+"_KJC_Sara", acqFunc)
             nexusTools.appendModelledMarkerFromAcq(NEXUS,subject,side+"_KJC_SaraAxis", acqFunc)
 
-            # --------------------------NEW MOTION FILTER - DISPLAY BONES---------  
+
+            #---Second model motion filter
+            # consider new anatomical frame
+
             modMotion=modelFilters.ModelMotionFilter(scp,acqFunc,model,pyCGM2Enums.motionMethod.Sodervisk)
             modMotion.segmentalCompute([proximalSegmentLabel,distalSegmentLabel])
+
+
+            # projection of the Sara axis in the transversale plane
+            projMeanAxis_inThigh = model.getSegment(proximalSegmentLabel).anatomicalFrame.getNodeTrajectory("proj_saraAxis")
+            btkTools.smartAppendPoint(acqFunc,side+"_proj_SaraAxis",projMeanAxis_inThigh)
+            nexusTools.appendModelledMarkerFromAcq(NEXUS,subject,side+"_proj_SaraAxis", acqFunc)
 
             # -- add nexus Bones
             if side == "Left":
                 nexusTools.appendBones(NEXUS,subject,acqFunc,"LFE1", model.getSegment("Left Thigh"),OriginValues = acqFunc.GetPoint("LKJC").GetValues() )
             elif side == "Right":
                 nexusTools.appendBones(NEXUS,subject,acqFunc,"RFE1", model.getSegment("Right Thigh"),OriginValues = acqFunc.GetPoint("RKJC").GetValues() )
-        
+
 
 
 
