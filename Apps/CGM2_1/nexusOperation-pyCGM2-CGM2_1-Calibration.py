54663244879cc3c7bcc21433cc06e559143225e1
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-07-13 13:37:57 +0100

    idem CGM2.1

--- Apps/CGM2_1/nexusOperation-pyCGM2-CGM2_1-Calibration.py
+++ Apps/CGM2_1/nexusOperation-pyCGM2-CGM2_1-Calibration.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     parser.add_argument('-l','--leftFlatFoot', type=int, help='left flat foot option')
     parser.add_argument('-r','--rightFlatFoot',type=int,  help='right flat foot option')
     parser.add_argument('-md','--markerDiameter', type=float, help='marker diameter')
+    parser.add_argument('-ps','--pointSuffix', type=str, help='suffix of model outputs')    
     parser.add_argument('--check', action='store_true', help='force model output suffix')
     args = parser.parse_args()
 
@@ -131,7 +132,10 @@ if __name__ == "__main__":
         if args.check:
             pointSuffix="cgm2.1"
         else:
-            pointSuffix = inputs["Global"]["Point suffix"]
+            if args.pointSuffix is not None:
+                pointSuffix = args.pointSuffix
+            else:
+                pointSuffix = inputs["Global"]["Point suffix"]
 
         # --------------------------ACQUISITION ------------------------------------
 
