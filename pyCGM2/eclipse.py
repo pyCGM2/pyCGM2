f9ccbff75cc7a641a098c64e2a75883b423ff0f4
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-08-02 14:06:55 +0100

    no need to match force plate for updating enf. Mapped force plate must be done before calling eclipse package method

--- pyCGM2/eclipse.py
+++ pyCGM2/eclipse.py
@@ -12,44 +12,39 @@ from pyCGM2.Tools import btkTools
 import configparser
 
 
-def enfForcePlateAssignment(c3dFullFilename):
+def enfForcePlateAssignment(c3dFullFilename,mappedForcePlate):
     """
-        Add Force plate assignement in the enf file 
+        Add Force plate assignement in the enf file
 
         :Parameters:
-            - `c3dFullFilename` (str) - filename with path of the c3d 
+            - `c3dFullFilename` (str) - filename with path of the c3d
     """
-        
+
     acqGait = btkTools.smartReader(str(c3dFullFilename))
     enfFile = str(c3dFullFilename[:-4]+".Trial.enf")
 
     if not os.path.isfile(enfFile):
         raise Exception ("[pyCGM2] - No enf file associated with the c3d")
-    else:            
-
-        # detect Force plate    
-        mappedForcePlate = forceplates.matchingFootSideOnForceplate(acqGait)
-        logging.info("Force plate assignment : %s" %mappedForcePlate)
-    
+    else:                
         # --------------------Modify ENF --------------------------------------
         configEnf = configparser.ConfigParser()
         configEnf.optionxform = str
         configEnf.read(enfFile)
-        
-        
+
+
         indexFP=1
         for letter in mappedForcePlate:
-            
-            if letter =="L": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Left"  
-            if letter =="R": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Right"  
-            if letter =="X": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Invalid"  
-         
+
+            if letter =="L": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Left"
+            if letter =="R": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Right"
+            if letter =="X": configEnf["TRIAL_INFO"]["FP"+str(indexFP)]="Invalid"
+
             indexFP+=1
-        
+
         tmpFile =str(c3dFullFilename[:-4]+".Trial.enf-tmp")
         with open(tmpFile, 'w') as configfile:
             configEnf.write(configfile)
-        
+
         os.remove(enfFile)
         os.rename(tmpFile,enfFile)
-        logging.warning("Enf file updated with Force plate assignment") 
+        logging.warning("Enf file updated with Force plate assignment")
