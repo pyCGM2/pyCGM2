dab79bc0cfadd31c0cf69cc0eb5fff984662c7ef
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-09-29 14:27:53 +0100

    attempt to create a new register key .. do not work so far

--- registry.py
+++ registry.py
@@ -10,13 +10,32 @@ def getPythonExeRegisterKey():
 
 
     value = winreg.QueryValue(reg_key, "")
-    
+
     return value
-    
+
 def setPythonExeRegisterKey(COMPATIBLENEXUSKEY):
-    
+
     reg_key = winreg.OpenKey(
         winreg.HKEY_CLASSES_ROOT,
         r'Applications\python.exe\shell\open\command',0,winreg.KEY_ALL_ACCESS)
 
     winreg.SetValueEx(reg_key, "" ,0, winreg.REG_SZ, COMPATIBLENEXUSKEY)
+
+
+def getPythonExeRegisterKey2():
+
+    try:
+        reg_key = winreg.OpenKey(
+            winreg.HKEY_CLASSES_ROOT,
+            r'Applications\python2.exe\shell\open\command',0,winreg.KEY_ALL_ACCESS)
+
+    value = winreg.QueryValue(reg_key, "")
+
+    return value
+
+
+# def createKey(COMPATIBLENEXUSKEY):
+# don t work !!
+#
+#     key = wreg.CreateKey(wreg.HKEY_CLASSES_ROOT, "Applications\\python2.exe\\shell\\open\\command")
+#     winreg.SetValueEx(reg_key, "" ,0, winreg.REG_SZ, COMPATIBLENEXUSKEY)
