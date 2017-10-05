7a6716d5f1e5066fbadf0b439f8e58159621430e
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-10-05 12:02:59 +0100

    new tests of the Nexus API

---
+++ Tests/Nexus/test_ActivateSubject.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import os
+import logging
+import matplotlib.pyplot as plt
+import json
+import pdb
+import cPickle
+import json
+from collections import OrderedDict
+import argparse
+
+# pyCGM2 settings
+import pyCGM2
+pyCGM2.CONFIG.setLoggingLevel(logging.INFO)
+
+
+# vicon nexus
+import ViconNexus
+
+
+
+
+# pyCGM2 libraries
+from pyCGM2.Tools import btkTools
+from pyCGM2.Nexus import nexusTools
+
+
+
+
+if __name__ == "__main__":
+
+    plt.close("all")
+    DEBUG = True
+
+    parser = argparse.ArgumentParser(description='CGM1 Calibration')
+    parser.add_argument('-l','--leftFlatFoot', type=int, help='left flat foot option')
+    parser.add_argument('-r','--rightFlatFoot',type=int,  help='right flat foot option')
+    parser.add_argument('-md','--markerDiameter', type=float, help='marker diameter')
+    parser.add_argument('-ps','--pointSuffix', type=str, help='suffix of model outputs')
+    parser.add_argument('--check', action='store_true', help='force model output suffix' )
+    args = parser.parse_args()
+
+    NEXUS = ViconNexus.ViconNexus()
+    NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+
+    if NEXUS_PYTHON_CONNECTED: # run Operation
+
+        # --------------------------GLOBAL SETTINGS ------------------------------------
+        # global setting ( in user/AppData)
+        inputs = json.loads(open(str(pyCGM2.CONFIG.PYCGM2_APPDATA_PATH+"CGM1-pyCGM2.settings")).read(),object_pairs_hook=OrderedDict)
+
+        # --------------------------LOADING ------------------------------------
+
+        if DEBUG:
+
+            DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\Clinics\\nerveBlock-Tibial Left\CGM1\\"
+            calibrateFilenameLabelledNoExt = "PATRE Eric Cal 02" #"PATRE Eric POST Cal 01" #"static Cal 01-noKAD-noAnkleMed" #
+
+            NEXUS.OpenTrial( str(DATA_PATH+calibrateFilenameLabelledNoExt), 30 )
+
+        else:
+            DATA_PATH, calibrateFilenameLabelledNoExt = NEXUS.GetTrialName()
+
+        calibrateFilenameLabelled = calibrateFilenameLabelledNoExt+".c3d"
+
+        logging.info( "data Path: "+ DATA_PATH )
+        logging.info( "calibration file: "+ calibrateFilenameLabelled)
+
+        # --------------------------SUBJECT ------------------------------------
+        subjects = NEXUS.GetSubjectNames()
+        subject = nexusTools.checkActivatedSubject(NEXUS,subjects)
+
+
+        if DEBUG:
+            NEXUS.SaveTrial(30)
+
+    else:
+        raise Exception("NO Nexus connection. Turn on Nexus")
