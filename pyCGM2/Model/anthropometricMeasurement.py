0b025a76b659c3761bbfb64f84bb6eed6f772cb3
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-11-08 15:46:02 +0000

    new module : Anthropometric Measurement and Timer

---
+++ pyCGM2/Model/anthropometricMeasurement.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def measureNorm(acq,MarkerLabel1,MarkerLabel2,markerDiameter=0):
+    mv1 = acq.GetPoint(MarkerLabel1).GetValues()
+    mv2 = acq.GetPoint(MarkerLabel2).GetValues()
+
+    n = acq.GetPointFrameNumber()
+    norm = np.linalg.norm(mv1-mv2, axis=1)
+
+    return norm.mean()-markerDiameter
---
+++ pyCGM2/Utils/timer.py
@@ -0,0 +1,29 @@
+import time
+
+
+class Timer(object):
+    """ A quick tic-toc timer
+
+        def usage1():
+        print 'Verbose mode'
+        with Timer('Usage 1'):
+            # do stuff
+            for _ in range(1000000):
+                pass
+    """
+
+    def __init__(self, name=None, verbose=True):
+        self.name = name
+        self.verbose = verbose
+        self.elapsed = None
+
+    def __enter__(self):
+        self.tstart = time.time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.elapsed = time.time() - self.tstart
+        if self.verbose:
+            if self.name:
+                print '[%s]' % self.name,
+            print 'Elapsed: %s' % (self.elapsed)
