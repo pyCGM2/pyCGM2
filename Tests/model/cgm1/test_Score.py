6f84858201ae0724a601ee330a898b3fb264c35f
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2017-12-01 16:00:17 +0000

    [new] add Score residuals

---
+++ Tests/model/cgm1/test_Score.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+
+import pyCGM2
+pyCGM2.CONFIG.setLoggingLevel(logging.INFO)
+
+import pyCGM2
+# btk
+pyCGM2.CONFIG.addBtk()
+
+# pyCGM2
+from pyCGM2 import enums
+from pyCGM2.Tools import  btkTools
+from pyCGM2.Model import  modelFilters,modelDecorator, frame, modelQualityFilter
+from pyCGM2.Model.CGM2 import cgm
+
+
+
+
+class CGM1():
+
+    @classmethod
+    def ModelScoreTest(cls):
+        """
+        GOAL : compare Joint centres and foot Offset
+
+        """
+        MAIN_PATH = pyCGM2.CONFIG.TEST_DATA_PATH + "CGM1\\PIG standard\\basic\\"
+        staticFilename = "MRI-US-01, 2008-08-08, 3DGA 02.c3d"
+
+        acqStatic = btkTools.smartReader(str(MAIN_PATH +  staticFilename))
+
+        model=cgm.CGM1LowerLimbs()
+        model.configure()
+
+        markerDiameter=14
+        mp={
+        'Bodymass'   : 71.0,
+        'LeftLegLength' : 860.0,
+        'RightLegLength' : 865.0 ,
+        'LeftKneeWidth' : 102.0,
+        'RightKneeWidth' : 103.4,
+        'LeftAnkleWidth' : 75.3,
+        'RightAnkleWidth' : 72.9,
+        'LeftSoleDelta' : 0,
+        'RightSoleDelta' : 0,
+        }
+        model.addAnthropoInputParameters(mp)
+
+        scp=modelFilters.StaticCalibrationProcedure(model)
+        modelFilters.ModelCalibrationFilter(scp,acqStatic,model).compute()
+
+        # ------ Test 1 Motion Axe X -------
+        gaitFilename="MRI-US-01, 2008-08-08, 3DGA 14.c3d"
+        acqGait = btkTools.smartReader(str(MAIN_PATH +  gaitFilename))
+
+
+        # Motion FILTER
+        # optimisation segmentaire et calibration fonctionnel
+        modMotion=modelFilters.ModelMotionFilter(scp,acqGait,model,enums.motionMethod.Determinist)
+        modMotion.compute()
+
+        # score
+
+        msrp = modelQualityFilter.ModelScoreResidualProcedure(model)
+        srf = modelQualityFilter.ScoreResidualFilter(acqGait,msrp)
+        srf.compute()
+
+    @classmethod
+    def GeneralScoreTest(cls):
+        """
+        GOAL : compare Joint centres and foot Offset
+
+        """
+        MAIN_PATH = pyCGM2.CONFIG.TEST_DATA_PATH + "CGM1\\PIG standard\\basic\\"
+        staticFilename = "MRI-US-01, 2008-08-08, 3DGA 02.c3d"
+
+        acqStatic = btkTools.smartReader(str(MAIN_PATH +  staticFilename))
+
+        model=cgm.CGM1LowerLimbs()
+        model.configure()
+
+        markerDiameter=14
+        mp={
+        'Bodymass'   : 71.0,
+        'LeftLegLength' : 860.0,
+        'RightLegLength' : 865.0 ,
+        'LeftKneeWidth' : 102.0,
+        'RightKneeWidth' : 103.4,
+        'LeftAnkleWidth' : 75.3,
+        'RightAnkleWidth' : 72.9,
+        'LeftSoleDelta' : 0,
+        'RightSoleDelta' : 0,
+        }
+        model.addAnthropoInputParameters(mp)
+
+        scp=modelFilters.StaticCalibrationProcedure(model)
+        modelFilters.ModelCalibrationFilter(scp,acqStatic,model).compute()
+
+        # ------ Test 1 Motion Axe X -------
+        gaitFilename="MRI-US-01, 2008-08-08, 3DGA 14.c3d"
+        acqGait = btkTools.smartReader(str(MAIN_PATH +  gaitFilename))
+
+
+        # Motion FILTER
+        # optimisation segmentaire et calibration fonctionnel
+        modMotion=modelFilters.ModelMotionFilter(scp,acqGait,model,enums.motionMethod.Determinist)
+        modMotion.compute()
+
+        # score
+
+        msrp = modelQualityFilter.GeneralScoreResidualProcedure(model)
+        msrp.setDefinition("LHJC","Pelvis","Left Thigh")
+
+        srf = modelQualityFilter.ScoreResidualFilter(acqGait,msrp)
+        srf.compute()
+
+
+if __name__ == "__main__":
+
+    #    plt.close("all")
+
+    CGM1.ModelScoreTest()
+    CGM1.GeneralScoreTest()
---
+++ pyCGM2/Model/modelQualityFilter.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import logging
+import numpy as np
+
+import btk
+
+
+from  pyCGM2.Math import numeric
+from pyCGM2.Tools import  btkTools
+
+
+
+class GeneralScoreResidualProcedure(object):
+
+    def __init__(self,model):
+
+        self.m_scoreDefinition = dict()
+        self.model = model
+
+    def setDefinition(self, nodeLabel, proxSegLabel, distSegLabel):
+        self.m_scoreDefinition[nodeLabel] = {"proximal": proxSegLabel , "distal": distSegLabel }
+
+
+class ModelScoreResidualProcedure(object):
+
+    def __init__(self, model):
+
+        self.model = model
+        self.m_scoreDefinition = dict()
+
+        for it in self.model.m_jointCollection:
+            self.m_scoreDefinition[it.m_nodeLabel] = {"proximal": it.m_proximalLabel , "distal": it.m_distalLabel }
+
+
+
+class ScoreResidualFilter(object):
+
+    def __init__(self,acq, scoreProcedure):
+        self.scoreProcedure = scoreProcedure
+        self.acq = acq
+
+        self.m_model = scoreProcedure.model
+
+    def compute(self):
+
+        for nodeLabel in self.scoreProcedure.m_scoreDefinition.keys():
+
+            proxLabel = self.scoreProcedure.m_scoreDefinition[nodeLabel]["proximal"]
+            distLabel = self.scoreProcedure.m_scoreDefinition[nodeLabel]["distal"]
+
+            proxNode =  self.m_model.getSegment(proxLabel).anatomicalFrame.getNodeTrajectory(nodeLabel)
+            distNode = self.m_model.getSegment(distLabel).anatomicalFrame.getNodeTrajectory(nodeLabel)
+            score = numeric.rms((proxNode-distNode),axis = 1)
+
+            scoreValues = np.array([score, np.zeros(self.acq.GetPointFrameNumber()), np.zeros(self.acq.GetPointFrameNumber())]).T
+
+            btkTools.smartAppendPoint(self.acq, str(nodeLabel+"_Score"),scoreValues, PointType=btk.btkPoint.Scalar,desc="Score")
