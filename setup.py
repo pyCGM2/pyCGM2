8599cf711b796b7b517f65617d3298cac292e04c
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2018-10-16 15:20:58 +0100

    update tag

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import site
 if sys.maxsize > 2**32:
     raise Exception ("64-bit python version detected. PyCGM2 requires a 32 bits python version")
 
-VERSION ="3.0.3"
+VERSION ="3.0.4"
 
 
 for it in site.getsitepackages():
