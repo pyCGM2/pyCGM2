3254420e2ccd8501817d1f1179226771545ac31c
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2019-01-25 15:05:07 +0000

    config tests in progress

--- Tests/configurator/configTests.py
+++ Tests/configurator/configTests.py
@@ -2,24 +2,25 @@
 import ipdb
 from pyCGM2.Utils import files
 import pyCGM2
+from pyCGM2.Configurator import Manager
 
 class tests():
 
     @classmethod
-    def yaml(cls):
-        DATA_PATH = "C:\\Users\\HLS501\\Documents\\Programming\\API\\pyCGM2\\pyCGM2\\SessionSettings\\globalSettings\\"
-        settings1_0_yaml = files.openFile(pyCGM2.PYCGM2_APPDATA_PATH,"CGM1-pyCGM2.ysettings")
-        settings1_0_json = files.openFile(pyCGM2.PYCGM2_APPDATA_PATH,"CGM1-pyCGM2.settings")
+    def testCGM1(cls):
+        DATA_PATH = "C:\\Users\\HLS501\\Documents\\Programming\\API\\pyCGM2\\pyCGM2\\SessionSettings\\"
+
+        internalSettings = files.openFile(DATA_PATH+"globalSettings\\","CGM1-pyCGM2.settings")
+
+        userSettings = files.openFile(DATA_PATH+"userSettings\\","CGM1.usettings")
+
+        manager = Manager.CGM1ConfigManager(userSettings)
+        manager.dynamicTrials
 
         ipdb.set_trace()
-        settings1_1 = files.openConfigurationFile(DATA_PATH,"CGM1_1-pyCGM2.ysettings")
-        settings2_1 = files.openConfigurationFile(DATA_PATH,"CGM2_1-pyCGM2.ysettings")
-        settings2_2 = files.openConfigurationFile(DATA_PATH,"CGM2_2-pyCGM2.ysettings")
-        settings2_3 = files.openConfigurationFile(DATA_PATH,"CGM2_3-pyCGM2.ysettings")
-        settings2_4 = files.openConfigurationFile(DATA_PATH,"CGM2_4-pyCGM2.ysettings")
 
 
 
 
 if __name__ == "__main__":
-    tests.yaml()
+    tests.testCGM1()
