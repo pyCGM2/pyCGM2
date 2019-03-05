d6e5634054110c66a0534e6ae8b7950164a031dd
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2019-03-05 16:39:40 +0000

    new init

--- pyCGM2/__init__.py
+++ pyCGM2/__init__.py
@@ -2,7 +2,28 @@ import sys
 import glob
 import re
 import os
+import logging
 
+def getLastNexusVersion():
+    nexusDir = "C:\Program Files (x86)\Vicon"
+    dirs = os.listdir(nexusDir)
+    li =[]
+    for it in dirs:
+        if "Nexus2" in it:
+            version = int(it[it.find(".")+1:])
+            li.append(version)
+    last = max(li)
+    return "Nexus2."+str(last)
+
+try:
+    NEXUS_VERSION = getLastNexusVersion()
+    if not "C:/Program Files (x86)/Vicon/"+NEXUS_VERSION+"/SDK/Python" in sys.path:
+        sys.path.append( "C:/Program Files (x86)/Vicon/"+NEXUS_VERSION+"/SDK/Python")
+
+    if not "C:/Program Files (x86)/Vicon/Nexus"+NEXUS_VERSION+"/SDK/Win32" in sys.path:
+        sys.path.append( "C:/Program Files (x86)/Vicon/"+NEXUS_VERSION+"/SDK/Win32")
+except Exception, errormsg:
+    logging.error (errormsg)
 
 # CONSTANTS
 cased_path = glob.glob(re.sub(r'([^:])(?=[/\\]|$)', r'[\1]', __file__))[0]
