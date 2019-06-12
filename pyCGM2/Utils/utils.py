b18bf07b406b92556c2615487b8d5e93069a4c13
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2019-06-12 15:17:11 +0100

    convenient is value in range

--- pyCGM2/Utils/utils.py
+++ pyCGM2/Utils/utils.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 def toBool(text):
     return True if text == "True" else False
+
+def isInRange(val, min, max):
+
+    if val<min or val>max:
+        return False
+    else:
+        return True
