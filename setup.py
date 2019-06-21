e2a3632085f3ef11ad6b06295e789eadedfef0af
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2019-06-21 15:06:34 +0100

    version updated

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if developMode:
 if sys.maxsize > 2**32:
     raise Exception ("64-bit python version detected. PyCGM2 requires a 32 bits python version")
 
-VERSION ="3.2.8"
+VERSION ="3.2.9"
 
 
 for it in site.getsitepackages():
