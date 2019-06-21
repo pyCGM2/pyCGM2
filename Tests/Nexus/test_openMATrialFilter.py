d7a5ce9bfafd4325c7125c60e6ce223c82a8784f
ISDADS\HLS501 <fabien.leboeuf@gmail.com>
2019-06-21 14:56:38 +0100

    new Tests for openMA:Trial creation with different scenarii

--- Tests/Nexus/test_openMATrialFilter.py
+++ Tests/Nexus/test_openMATrialFilter.py
@@ -148,13 +148,215 @@ class Tests():
         ma.io.write(root,str("modelOutputsOpenMA_checked.c3d"))
 
 
+class TestsX2d():
+
+
+    @classmethod
+    def noCroppedC3d(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_withx2d\\"
+        filenameNoExt = "gait_noCropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsX2d_noCroppedC3d_openma.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, str(filenameNoExt+".c3d"))
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+
+
+    @classmethod
+    def croppedC3d(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_withx2d\\"
+        filenameNoExt = "gait_cropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsX2d_croppedC3d_openma.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, str(filenameNoExt+".c3d"))
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+    @classmethod
+    def interactiveCropping_fromNoCropped(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_withx2d\\"
+        filenameNoExt = "gait_noCropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        NEXUS.SetTrialRegionOfInterest(300, 400)
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsNOX2d_interactiveCropping_fromNoCropped_openMA.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, "forCheckingInteractiveCropped\\gait_Cropped - 300-400.c3d")
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+    @classmethod
+    def interactiveCropping_fromCropped(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_withx2d\\"
+        filenameNoExt = "gait_cropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        NEXUS.SetTrialRegionOfInterest(300, 400)
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsNOX2d_interactiveCropping_fromCropped_openMA.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, "forCheckingInteractiveCropped\\gait_Cropped - 300-400.c3d")
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+
+class TestsNOX2d():
+
+
+    @classmethod
+    def noCroppedC3d(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_NOx2d\\"
+        filenameNoExt = "gait_noCropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsNOX2d_noCroppedC3d_openma.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, str(filenameNoExt+".c3d"))
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+
+
+    @classmethod
+    def croppedC3d(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_NOx2d\\"
+        filenameNoExt = "gait_cropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsNOX2d_croppedC3d_openma.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, str(filenameNoExt+".c3d"))
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+    @classmethod
+    def interactiveCropping_fromNoCropped(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_NOx2d\\"
+        filenameNoExt = "gait_noCropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        NEXUS.SetTrialRegionOfInterest(300, 400)
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsNOX2d_interactiveCropping_fromNoCropped_openMA.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, "forCheckingInteractiveCropped\\gait_Cropped - 300-400.c3d")
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+    @classmethod
+    def interactiveCropping_fromCropped(cls):
+        NEXUS = ViconNexus.ViconNexus()
+        NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
+
+        DATA_PATH ="C:\\Users\\HLS501\\Documents\\VICON DATA\\pyCGM2-Data\\NexusAPI\\BtkAcquisitionCreator\\sample_NOx2d\\"
+        filenameNoExt = "gait_cropped"
+        NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+        NEXUS.SetTrialRegionOfInterest(300, 400)
+        subject = NEXUS.GetSubjectNames()[0]
+
+        trialConstructorFilter = nexusFilters.NexusConstructTrialFilter(DATA_PATH,filenameNoExt,subject)
+        trial = trialConstructorFilter.build()
+
+        ma.io.write(trialConstructorFilter.m_root,"TestsNOX2d_interactiveCropping_fromCropped_openMA.c3d")
+
+        trial0 = trialTools.smartTrialReader(DATA_PATH, "forCheckingInteractiveCropped\\gait_Cropped - 300-400.c3d")
+
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"LTHI").data(),
+                                            trial.findChild(ma.T_TimeSequence,"LTHI").data(),decimal=2)
+        np.testing.assert_array_almost_equal(trial0.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),
+                                            trial.findChild(ma.T_TimeSequence,"Voltage.EMG1").data(),decimal=2)
+
+
 
 
 
 
 if __name__ == "__main__":
 
-    Tests.croppedC3d()
-    Tests.croppedC3d_noX2d()
-    Tests.noCroppedC3d()
-    Tests.modelOutputs()
+    # Tests.croppedC3d()
+    # Tests.croppedC3d_noX2d()
+    # Tests.noCroppedC3d()
+    # Tests.modelOutputs()
+
+    # TestsX2d.noCroppedC3d()
+    # TestsX2d.croppedC3d()
+    # TestsX2d.interactiveCropping_fromNoCropped()
+    # TestsX2d.interactiveCropping_fromCropped()
+
+    TestsNOX2d.noCroppedC3d()
+    TestsNOX2d.croppedC3d()
+    TestsNOX2d.interactiveCropping_fromNoCropped()
+    TestsNOX2d.interactiveCropping_fromCropped()
