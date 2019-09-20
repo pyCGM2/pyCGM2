cb27655e34aaa711af2e8f10f9f760d406ce8597
Leboeuf Fabien <F.Leboeuf@salford.ac.uk>
2019-09-20 13:10:47 +0200

    update of exporter tes

--- Tests/processing/exporterTest.py
+++ Tests/processing/exporterTest.py
@@ -33,91 +33,18 @@ class ExportTest():
 
         #---- Analysis
         #--------------------------------------------------------------------------
-
         # ----INFOS-----
         modelInfo={"Type":"cgm2", "hjc":"hara"}
         subjectInfo={"Id":"1", "Name":"Lecter"}
         experimentalInfo={"Condition":"Barefoot", "context":"block"}
 
-        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis("Gait",  DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
+        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis(DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
 
 
         exportFilter = exporter.XlsAnalysisExportFilter()
         exportFilter.setAnalysisInstance(analysisInstance)
-        exportFilter.export("testAdvanced", path=DATA_PATH,excelFormat = "xls",mode="Advanced")
+        exportFilter.export("advancedExport", path=None,excelFormat = "xls",mode="Advanced")
 
-    @classmethod
-    def analysisAdvancedAndBasic_nonExistingLabel(cls):
-
-        # ----DATA-----
-        DATA_PATH = pyCGM2.TEST_DATA_PATH+"operations\\analysis\\gait\\"
-        modelledFilenames = ["gait Trial 03 - viconName.c3d","gait Trial 01 - viconName.c3d" ]
-
-        #---- c3d manager
-        #--------------------------------------------------------------------------
-
-        c3dmanagerProcedure = c3dManager.UniqueC3dSetProcedure(DATA_PATH,modelledFilenames)
-        cmf = c3dManager.C3dManagerFilter(c3dmanagerProcedure)
-        cmf.enableEmg(False)
-        trialManager = cmf.generate()
-
-
-
-        #---- GAIT CYCLES FILTER
-        #--------------------------------------------------------------------------
-        cycleBuilder = cycle.GaitCyclesBuilder(spatioTemporalTrials=trialManager.spatioTemporal["Trials"],
-                                                   kinematicTrials = trialManager.kinematic["Trials"],
-                                                   kineticTrials = trialManager.kinetic["Trials"],
-                                                   emgTrials=trialManager.emg["Trials"])
-
-        cyclefilter = cycle.CyclesFilter()
-        cyclefilter.setBuilder(cycleBuilder)
-        cycles = cyclefilter.build()
-
-
-        #---- GAIT ANALYSIS FILTER
-        #--------------------------------------------------------------------------
-
-        # ----INFOS-----
-        modelInfo={"type":"S01"}
-        subjectInfo=None
-        experimentalInfo=None
-
-        kinematicLabelsDict ={ 'Left': ["LHipAngles","LKneeAngles","LAnkleAngles","LForeFootAngles"],
-                        'Right': ["RHipAngles","RKneeAngles","RAnkleAngles"] }
-
-        kineticLabelsDict =None#{ 'Left': ["LHipMoment","LKneeMoment"],
-                             #'Right': ["RHipMoment","RKneeMoment"]}
-
-
-        analysisBuilder = analysis.GaitAnalysisBuilder(cycles,
-                                                      kinematicLabelsDict = kinematicLabelsDict,
-                                                      kineticLabelsDict = kineticLabelsDict,
-                                                      subjectInfos=subjectInfo,
-                                                      modelInfos=modelInfo,
-                                                      experimentalInfos=experimentalInfo)
-
-        analysisFilter = analysis.AnalysisFilter()
-        analysisFilter.setBuilder(analysisBuilder)
-        analysisFilter.setInfo(model = modelInfo)
-        analysisFilter.build()
-
-        analysisInstance = analysisFilter.analysis
-
-        # ----INFOS-----
-        modelInfo={"Type":"cgm2", "hjc":"hara"}
-        subjectInfo={"Id":"1", "Name":"Lecter"}
-        experimentalInfo={"Condition":"Barefoot", "context":"block"}
-
-
-
-        exportFilter = exporter.XlsAnalysisExportFilter()
-        exportFilter.setAnalysisInstance(analysisInstance)
-        exportFilter.export("testAdvancedNan", path=DATA_PATH,excelFormat = "xls",mode="Advanced")
-
-        exportFilter2 = exporter.XlsAnalysisExportFilter()
-        exportFilter2.setAnalysisInstance(analysisInstance)
-        exportFilter2.export("testBasic2", path=DATA_PATH,excelFormat = "xls",mode="Basic")
 
     @classmethod
     def analysisBasic(cls):
@@ -143,12 +70,12 @@ class ExportTest():
         experimentalInfo={"Condition":"Barefoot", "context":"block"}
 
 
-        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis("Gait", DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
+        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis(DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
 
 
         exportFilter = exporter.XlsAnalysisExportFilter()
         exportFilter.setAnalysisInstance(analysisInstance)
-        exportFilter.export("test", path=DATA_PATH,excelFormat = "xls",mode="Basic")
+        exportFilter.export("basicExport", path=None,excelFormat = "xls",mode="Basic")
 
 
     @classmethod
@@ -175,12 +102,12 @@ class ExportTest():
         subjectInfo={"Id":"1", "Name":"Lecter"}
         experimentalInfo={"Condition":"Barefoot", "context":"block"}
 
-        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis("Gait", DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
+        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis(DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
 
 
         exportFilter = exporter.AnalysisExportFilter()
         exportFilter.setAnalysisInstance(analysisInstance)
-        exportFilter.export("testAdvanced", path=DATA_PATH)
+        exportFilter.export("jsonExport", path=None)
 
     @classmethod
     def analysisC3d(cls):
@@ -206,12 +133,12 @@ class ExportTest():
         subjectInfo={"Id":"1", "Name":"Lecter"}
         experimentalInfo={"Condition":"Barefoot", "context":"block"}
 
-        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis("Gait", DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
+        analysisInstance = pyCGM2.Lib.analysis.makeAnalysis(DATA_PATH,modelledFilenames,subjectInfo, experimentalInfo, modelInfo)
 
 
         exportFilter = exporter.AnalysisC3dExportFilter()
         exportFilter.setAnalysisInstance(analysisInstance)
-        exportFilter.export("testAdvanced", path=DATA_PATH)
+        exportFilter.export("c3dExport", path=None)
 
     @classmethod
     def analysisAdvancedEMG(cls):
@@ -234,23 +161,22 @@ class ExportTest():
 
         exportFilter = exporter.XlsAnalysisExportFilter()
         exportFilter.setAnalysisInstance(emgAnalysis)
-        exportFilter.export("testAdvancedEMG", path=DATA_PATH,excelFormat = "xls",mode="Advanced")
+        exportFilter.export("Emg_advancedExport", path=None,excelFormat = "xls",mode="Advanced")
 
-        # exportFilter = exporter.AnalysisExportFilter()
-        # exportFilter.setAnalysisInstance(emgAnalysis)
-        # exportFilter.export("testAdvancedEMG.json", path=DATA_PATH)
+        exportFilter = exporter.AnalysisExportFilter()
+        exportFilter.setAnalysisInstance(emgAnalysis)
+        exportFilter.export("Emg_export.json", path=None)
 
         exportFilter = exporter.AnalysisC3dExportFilter()
         exportFilter.setAnalysisInstance(emgAnalysis)
-        exportFilter.export("testAdvanced", path=DATA_PATH)
+        exportFilter.export("emg_C3dExport", path=None)
 
 if __name__ == "__main__":
 
     plt.close("all")
 
-    #ExportTest.analysisAdvanced()
-    # ExportTest.analysisAdvancedAndBasic_nonExistingLabel()
-    # ExportTest.analysisBasic()
-    #ExportTest.analysisJson()
-    #ExportTest.analysisC3d()
+    ExportTest.analysisAdvanced()
+    ExportTest.analysisBasic()
+    ExportTest.analysisJson()
+    ExportTest.analysisC3d()
     ExportTest.analysisAdvancedEMG()
