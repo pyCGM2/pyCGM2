dbf79ed5e3e8231a7ac4d8f4e2fa4f2935fc07b4
Leboeuf Fabien <F.Leboeuf@salford.ac.uk>
2019-09-20 13:16:51 +0200

    wrong version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if developMode:
 if sys.maxsize > 2**32:
     raise Exception ("64-bit python version detected. PyCGM2 requires a 32 bits python version")
 
-VERSION ="3.2.11"
+VERSION ="3.2.10"
 
 
 for it in site.getsitepackages():
