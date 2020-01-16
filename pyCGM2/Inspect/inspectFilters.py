58cc1cbb30b0a100ddadb2c898e12e37412e5812
Fabien Leboeuf <aaa34169@users.noreply.github.com>
2020-01-16 16:22:45 +0100

    inspectors log improved

--- pyCGM2/Inspect/inspectFilters.py
+++ pyCGM2/Inspect/inspectFilters.py
@@ -9,17 +9,25 @@ from pyCGM2.Tools import btkTools
 class QualityFilter(object):
     def __init__(self,procedure):
         self.m_procedure = procedure
+        self.exceptionMode = False
 
     def enableExceptionMode(self):
         self.m_procedure.exceptionMode = True
+        self.exceptionMode = True
 
     def disableExceptionMode(self):
-        self.m_procedure.exceptionMode = False        
+        self.m_procedure.exceptionMode = False
+        self.exceptionMode = False
 
 
     def run(self):
+        logging.info("----Quality test: %s ----"%(self.m_procedure.title))
         self.m_procedure.check()
         if self.m_procedure.state:
-            logging.info("quality Test => OK :-)")
+            logging.info("quality Test  => OK :-)")
         else :
-            logging.info("quality Test => FAILED :-(")
+            if not self.exceptionMode:
+                logging.info("quality Test => FAILED :-(")
+            else:
+                raise Exception ("[pyCGM2] Quality test (%s) fails"%(self.m_procedure.title))
+        logging.info("--------------------------")
