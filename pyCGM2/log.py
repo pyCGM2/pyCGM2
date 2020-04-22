42f441f2c96c4dee455856963387a9f2a8fd9f22
Fabien Leboeuf <aaa34169@users.noreply.github.com>
2020-04-22 12:53:35 +0200

    log file : mode eras activated by default

--- pyCGM2/log.py
+++ pyCGM2/log.py
@@ -8,13 +8,16 @@ from logging import handlers
 def setLoggingLevel(level):
     logging.basicConfig(format = "[pyCGM2-%(levelname)s]-%(module)s-%(funcName)s : %(message)s",level = level)
 
-def setLogger(filename = "pyCGM2.log",level = logging.INFO ):
+def setLogger(filename = "pyCGM2.log",level = logging.INFO,mode="erase" ):
     log = logging.getLogger('')
     log.setLevel(level)
     format = logging.Formatter("[pyCGM2-%(levelname)s]-%(module)s-%(funcName)s : %(message)s")
     ch = logging.StreamHandler(sys.stdout)
     ch.setFormatter(format)
     log.addHandler(ch)
-    fh = handlers.RotatingFileHandler(filename, maxBytes=(1048576*5), backupCount=7)
+    if mode =="erase":
+        fh = logging.FileHandler(filename, mode='w')
+    else:
+        fh = handlers.RotatingFileHandler(filename, maxBytes=(1048576*5), backupCount=7)
     fh.setFormatter(format)
     log.addHandler(fh)
