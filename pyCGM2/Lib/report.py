41f8e45b5e5b1acb17cdc5a3bdc5ecd25e29227e
Fabien Leboeuf <aaa34169@users.noreply.github.com>
2020-04-23 13:38:17 +0200

    multipage pdf

--- pyCGM2/Lib/report.py
+++ pyCGM2/Lib/report.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from pyCGM2.Lib import analysis
 from pyCGM2.Lib import plot
 from pyCGM2 import enums
@@ -14,78 +15,85 @@ def pdfGaitReport(DATA_PATH,model,modelledTrials, normativeDataset,pointSuffix, 
         modelInfo=None,
         pointLabelSuffix=None)
 
-    title = type
-
-    # spatiotemporal
-    plot.plot_spatioTemporal(DATA_PATH,analysisInstance,
-        exportPdf=True,
-        outputName=title,
-        show=None,
-        title=title)
-
-    #Kinematics
-    if model.m_bodypart in [enums.BodyPart.LowerLimb,enums.BodyPart.LowerLimbTrunk, enums.BodyPart.FullBody]:
-        plot.plot_DescriptiveKinematic(DATA_PATH,analysisInstance,"LowerLimb",
-            normativeDataset,
-            exportPdf=True,
+    with PdfPages(DATA_PATH + title+".pdf") as pdf:
+        # spatiotemporal
+        plot.plot_spatioTemporal(DATA_PATH,analysisInstance,
+            exportPdf=False,
             outputName=title,
-            pointLabelSuffix=pointSuffix,
-            show=False,
+            show=None,
             title=title)
 
-        plot.plot_ConsistencyKinematic(DATA_PATH,analysisInstance,"LowerLimb",
+        #Kinematics
+        if model.m_bodypart in [enums.BodyPart.LowerLimb,enums.BodyPart.LowerLimbTrunk, enums.BodyPart.FullBody]:
+            plot.plot_DescriptiveKinematic(DATA_PATH,analysisInstance,"LowerLimb",
+                normativeDataset,
+                exportPdf=False,
+                outputName=title,
+                pointLabelSuffix=pointSuffix,
+                show=False,
+                title=title)
+            pdf.savefig()
+
+            plot.plot_ConsistencyKinematic(DATA_PATH,analysisInstance,"LowerLimb",
+                normativeDataset,
+                exportPdf=False,
+                outputName=title,
+                pointLabelSuffix=pointSuffix,
+                show=False,
+                title=title)
+            pdf.savefig()
+
+        if model.m_bodypart in [enums.BodyPart.LowerLimbTrunk, enums.BodyPart.FullBody]:
+            plot.plot_DescriptiveKinematic(DATA_PATH,analysisInstance,"Trunk",
+                normativeDataset,
+                exportPdf=False,
+                outputName=title,
+                pointLabelSuffix=pointSuffix,
+                show=False,
+                title=title)
+            pdf.savefig()
+
+            plot.plot_ConsistencyKinematic(DATA_PATH,analysisInstance,"Trunk",
+                normativeDataset,
+                exportPdf=False,
+                outputName=title,
+                pointLabelSuffix=pointSuffix,
+                show=False,
+                title=title)
+            pdf.savefig()
+
+        if model.m_bodypart in [enums.BodyPart.UpperLimb, enums.BodyPart.FullBody]:
+            pass # TODO plot upperlimb panel
+
+
+        #Kinetics
+        if model.m_bodypart in [enums.BodyPart.LowerLimb,enums.BodyPart.LowerLimbTrunk, enums.BodyPart.FullBody]:
+            plot.plot_DescriptiveKinetic(DATA_PATH,analysisInstance,"LowerLimb",
+                normativeDataset,
+                exportPdf=False,
+                outputName=title,
+                pointLabelSuffix=pointSuffix,
+                show=False,
+                title=title)
+            pdf.savefig()
+
+            plot.plot_ConsistencyKinetic(DATA_PATH,analysisInstance,"LowerLimb",
+                normativeDataset,
+                exportPdf=False,
+                outputName=title,
+                pointLabelSuffix=pointSuffix,
+                show=False,
+                title=title)
+            pdf.savefig()
+
+        #MAP
+        plot.plot_MAP(DATA_PATH,analysisInstance,
             normativeDataset,
-            exportPdf=True,
-            outputName=title,
-            pointLabelSuffix=pointSuffix,
+            exportPdf=False,
+            outputName=title,pointLabelSuffix=pointSuffix,
             show=False,
             title=title)
-    if model.m_bodypart in [enums.BodyPart.LowerLimbTrunk, enums.BodyPart.FullBody]:
-        plot.plot_DescriptiveKinematic(DATA_PATH,analysisInstance,"Trunk",
-            normativeDataset,
-            exportPdf=True,
-            outputName=title,
-            pointLabelSuffix=pointSuffix,
-            show=False,
-            title=title)
+        pdf.savefig()
 
-        plot.plot_ConsistencyKinematic(DATA_PATH,analysisInstance,"Trunk",
-            normativeDataset,
-            exportPdf=True,
-            outputName=title,
-            pointLabelSuffix=pointSuffix,
-            show=False,
-            title=title)
-
-    if model.m_bodypart in [enums.BodyPart.UpperLimb, enums.BodyPart.FullBody]:
-        pass # TODO plot upperlimb panel
-
-
-    #Kinetics
-    if model.m_bodypart in [enums.BodyPart.LowerLimb,enums.BodyPart.LowerLimbTrunk, enums.BodyPart.FullBody]:
-        plot.plot_DescriptiveKinetic(DATA_PATH,analysisInstance,"LowerLimb",
-            normativeDataset,
-            exportPdf=True,
-            outputName=title,
-            pointLabelSuffix=pointSuffix,
-            show=False,
-            title=title)
-
-        plot.plot_ConsistencyKinetic(DATA_PATH,analysisInstance,"LowerLimb",
-            normativeDataset,
-            exportPdf=True,
-            outputName=title,
-            pointLabelSuffix=pointSuffix,
-            show=False,
-            title=title)
-
-    #MAP
-    plot.plot_MAP(DATA_PATH,analysisInstance,
-        normativeDataset,
-        exportPdf=True,
-        outputName=title,pointLabelSuffix=pointSuffix,
-        show=False,
-        title=title)
-
-    plt.show(False)
-    logging.info("----- Gait Processing -----> DONE")
+        plt.show(False)
+        logging.info("----- Gait Processing -----> DONE")
