7ef19a168bd2b2c2f3b1a0c78a201bc6cf7a3374
fleb <fabien.leboeuf@gmail.com>
2020-09-23 14:28:48 +0200

    FIX - oups wrong btk methods

--- pyCGM2/Processing/cycle.py
+++ pyCGM2/Processing/cycle.py
@@ -214,8 +214,8 @@ class Cycle(object):
 
         self.acq=acq
 
-        self.pointfrequency = float(acq.GetPointFrame())
-        self.analogfrequency = float(acq.GetAnalogFrame())
+        self.pointfrequency = float(acq.GetPointFrequency())
+        self.analogfrequency = float(acq.GetAnalogFrequency())
 
         #self.pointfrequency = float(btkTools.smartGetMetadata(self.acq,"POINT","RATE")[0]) #trial.findChild(ma.T_TimeSequence,"",[["type",ma.TimeSequence.Type_Marker]]).sampleRate()
         #self.analogfrequency = float(btkTools.smartGetMetadata(self.acq,"ANALOG","RATE")[0]) #trial.findChild(ma.T_TimeSequence,"",[["type",ma.TimeSequence.Type_Analog]]).sampleRate()
