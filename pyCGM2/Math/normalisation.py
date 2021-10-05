023f6ec922ea25813d7a82ee3b97e804e732fb44
fleb <fabien.leboeuf@gmail.com>
2021-10-05 16:29:54 +0200

    fix - management of flat array

--- pyCGM2/Math/normalisation.py
+++ pyCGM2/Math/normalisation.py
@@ -11,10 +11,18 @@ def timeSequenceNormalisation(Nrow,data):
             - `data` (numpy.array(m,n)) : number of interval
 
     """
+    if len(data.shape) == 1:
+        flatArray = True
+        data = data.reshape((data.shape[0],1))
+    else:
+        flatArray=False
 
     ncol = data.shape[1]
     out=np.zeros((Nrow,ncol))
     for i in range(0,ncol):
         out[:,i] = np.interp(np.linspace(0, 100, Nrow), np.linspace(0, 100, data.shape[0]), data[:,i])
+
+    if flatArray:
+        out = out.reshape(out.shape[0])
 
     return out
