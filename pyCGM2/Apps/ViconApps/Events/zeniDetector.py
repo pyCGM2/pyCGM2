39d542f57e200f7414cbdd6b63a90131e0460f6e
fleb <fabien.leboeuf@gmail.com>
2021-12-10 09:50:30 +0100

    doc

--- pyCGM2/Apps/ViconApps/Events/zeniDetector.py
+++ pyCGM2/Apps/ViconApps/Events/zeniDetector.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
-import pyCGM2; LOGGER = pyCGM2.LOGGER
-import argparse
 
-# pyCGM2 settings
-import pyCGM2
-
-# vicon nexus
-from viconnexusapi import ViconNexus
-
-
-# pyCGM2 libraries
+from pyCGM2.Nexus import nexusTools, nexusFilters
 from pyCGM2.Lib import eventDetector
-from pyCGM2.Nexus import nexusTools,nexusFilters
+from viconnexusapi import ViconNexus
+import argparse
+import pyCGM2
+LOGGER = pyCGM2.LOGGER
+
 
 def main():
     """  Run Zeni's kinematic-basd event detector on the  nexus-loaded trial
@@ -30,16 +25,16 @@ def main():
     """
 
     parser = argparse.ArgumentParser(description='ZeniDetector')
-    parser.add_argument('-fso','--footStrikeOffset', type=int, help='systenatic foot strike offset on both side')
-    parser.add_argument('-foo','--footOffOffset', type=int, help='systenatic foot off offset on both side')
+    parser.add_argument('-fso', '--footStrikeOffset', type=int,
+                        help='systenatic foot strike offset on both side')
+    parser.add_argument('-foo', '--footOffOffset', type=int,
+                        help='systenatic foot off offset on both side')
     args = parser.parse_args()
-
 
     NEXUS = ViconNexus.ViconNexus()
     NEXUS_PYTHON_CONNECTED = NEXUS.Client.IsConnected()
 
-
-    if NEXUS_PYTHON_CONNECTED: # run Operation
+    if NEXUS_PYTHON_CONNECTED:  # run Operation
 
         # ----------------------INPUTS-------------------------------------------
         # --- acquisition file and path----
@@ -47,8 +42,8 @@ def main():
 
         reconstructFilenameLabelled = reconstructFilenameLabelledNoExt+".c3d"
 
-        LOGGER.logger.info( "data Path: "+ DATA_PATH )
-        LOGGER.logger.info( "calibration file: "+ reconstructFilenameLabelled)
+        LOGGER.logger.info("data Path: " + DATA_PATH)
+        LOGGER.logger.info("calibration file: " + reconstructFilenameLabelled)
 
         #acqGait = btkTools.smartReader(str(DATA_PATH + reconstructFilenameLabelled))
 
@@ -56,33 +51,33 @@ def main():
 
         # Notice : Work with ONE subject by session
         subject = nexusTools.getActiveSubject(NEXUS)
-        LOGGER.logger.info(  "Subject name : " + subject  )
+        LOGGER.logger.info("Subject name : " + subject)
 
         # --- btk acquisition ----
-        nacf = nexusFilters.NexusConstructAcquisitionFilter(DATA_PATH,reconstructFilenameLabelledNoExt,subject)
+        nacf = nexusFilters.NexusConstructAcquisitionFilter(
+            DATA_PATH, reconstructFilenameLabelledNoExt, subject)
         acqGait = nacf.build()
-
 
         # ----------------------EVENT DETECTOR-------------------------------
         footStrikeOffset = 0
-        footOffOffset=0
+        footOffOffset = 0
         if args.footStrikeOffset is not None:
             footStrikeOffset = args.footStrikeOffset
         if args.footOffOffset is not None:
             footOffOffset = args.footOffOffset
 
-        eventDetector.zeni(acqGait,footStrikeOffset=footStrikeOffset,footOffOffset=footOffOffset)
+        eventDetector.zeni(
+            acqGait, footStrikeOffset=footStrikeOffset, footOffOffset=footOffOffset)
 
         # ----------------------DISPLAY ON VICON-------------------------------
-        nexusTools.createEvents(NEXUS,subject,acqGait,["Foot Strike","Foot Off"])
+        nexusTools.createEvents(NEXUS, subject, acqGait, [
+                                "Foot Strike", "Foot Off"])
         # ========END of the nexus OPERATION if run from Nexus  =========
-
-
 
     else:
         raise Exception("NO Nexus connection. Turn on Nexus")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
 
     main()
