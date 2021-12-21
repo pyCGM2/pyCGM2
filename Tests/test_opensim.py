f6dece5b538350ba83543db98b15f3e5cf2f370f
fleb <fabien.leboeuf@gmail.com>
2021-12-21 16:27:39 +0100

    test of opensimIO

--- Tests/test_opensim.py
+++ Tests/test_opensim.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# pytest -s --disable-pytest-warnings  test_opensim.py::Test_misc::test_foot_grf_file
+# pytest -s --disable-pytest-warnings  test_opensim.py::Test_IO::test_motFile
 from pyCGM2.Processing import progressionFrame
 from pyCGM2.ForcePlates import forceplates
 from pyCGM2.Model.Opensim import osimProcessing
 from pyCGM2.Model.Opensim import opensimFilters
+from pyCGM2.Model.Opensim import opensimIO
 from pyCGM2 import enums
 from pyCGM2.Model import modelFilters, modelDecorator
 from pyCGM2.Model.CGM2 import decorators
@@ -35,6 +36,26 @@ class Test_basics:
         DATA_PATH = pyCGM2.TEST_DATA_PATH + "OpenSim/CGM23\\gait\\"
 
         opensimTools.createGroundReactionForceMOT_file(DATA_PATH, 'gait1.c3d')
+
+
+class Test_IO:
+    def test_motFile(self):
+
+
+        DATA_PATH = pyCGM2.TEST_DATA_PATH + "OpenSim/IO\\"
+
+        motDf = opensimIO.OpensimDataFrame(DATA_PATH, "gait1.mot")
+        motDf.getDataFrame()["pelvis_tilt"] = 0.0
+        motDf.save(filename="gait1_2.mot")
+
+
+    def test_stoFile(self):
+
+        DATA_PATH = pyCGM2.TEST_DATA_PATH + "OpenSim/IO\\"
+        stoDf = opensimIO.OpensimDataFrame(DATA_PATH, "cgm2-Osim-scaled_MuscleAnalysis_ActiveFiberForce.sto")
+
+
+
 
 
 class Test_misc:
