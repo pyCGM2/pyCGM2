e0a2f0b12d5cbd436d0f514ffbb719444e997a46
fleb <fabien.leboeuf@gmail.com>
2022-01-07 17:39:02 +0100

    doc

--- pyCGM2/Anomaly/AnomalyFilter.py
+++ pyCGM2/Anomaly/AnomalyFilter.py
@@ -1,27 +1,66 @@
 # -*- coding: utf-8 -*-
-import pyCGM2; LOGGER = pyCGM2.LOGGER
+#APIDOC: /Low level/Anomaly
+
+""" This module contains pyCGM2 anomaly fiters to deal with either the anomaly detector procedure or
+the anomaly correction procedure
+
+check out the script : *\Tests\test_anomalies.py* for examples
+
+"""
+
+import pyCGM2
+LOGGER = pyCGM2.LOGGER
+
 
 class AnomalyDetectionFilter(object):
-    def __init__(self,acq,filename,procedure,**kwargs):
+    """ anomaly detector filter
+    """
+
+    def __init__(self, acq, filename, procedure, **kwargs):
+        """ constructor
+
+        Args:
+            acq (btk.Acquisition): a btk acquisition instantce
+            filename (str): filename
+            procedure(pyCGM2.Anomaly.AnomalyDetectionProcedure): anomaly detector procedure instance
+
+        Low-level Keyword Args :
+            frameRange ([int, int]) frame boundaries
+
+
+        """
         self.m_procedure = procedure
         self.m_acq = acq
         self.m_filename = filename
         self.options = kwargs
 
     def run(self):
-        self.m_procedure.run(self.m_acq,self.m_filename,self.options)
+        """ run the filter
+        """
+        self.m_procedure.run(self.m_acq, self.m_filename, self.options)
         anomaly = self.m_procedure.getAnomaly()
         return anomaly
 
 
 class AnomalyCorrectionFilter(object):
-    def __init__(self,acq,filename,procedure):
+    """ anomaly corrector filter"""
+
+    def __init__(self, acq, filename, procedure):
+        """ constructor
+
+        Args:
+            acq (btk.Acquisition): a btk acquisition instantce
+            filename (str): filename
+            procedure(pyCGM2.Anomaly.AnomalyDetectionProcedure): anomaly correction procedure instance
+
+        """
         self.m_procedure = procedure
         self.m_acq = acq
         self.m_filename = filename
 
     def run(self):
-
-        out = self.m_procedure.run(self.m_acq,self.m_filename)
+        """ run the fiter
+        """
+        out = self.m_procedure.run(self.m_acq, self.m_filename)
 
         return out
