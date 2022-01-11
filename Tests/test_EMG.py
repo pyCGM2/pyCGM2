637530fa0caf06b4d736f57ef4fc955d1ba27827
fleb <fabien.leboeuf@gmail.com>
2022-01-11 11:45:36 +0100

    add coactivation tests

--- Tests/test_EMG.py
+++ Tests/test_EMG.py
@@ -1,5 +1,7 @@
-# coding: utf-8
-# pytest -s --log-cli-level=INFO --disable-pytest-warnings  test_EMG.py::Test_EMG::test_MVC
+# -*- coding: utf-8 -*-
+# @Author: Fabien Leboeuf
+
+# pytest -s --log-cli-level=INFO --disable-pytest-warnings  test_EMG.py::Test_EMG::test_Coactivation
 import os
 import pytest
 import numpy as np
@@ -8,6 +10,8 @@ import matplotlib.pyplot as plt
 from pyCGM2.Lib import analysis
 from pyCGM2.Lib import emg
 from pyCGM2.Utils import files
+from pyCGM2.EMG import coactivation
+from pyCGM2.EMG import emgFilters
 
 class Test_EMG:
     def test_MVC(self):
@@ -65,3 +69,67 @@ class Test_EMG:
         # plt.plot(emgAnalysisInstance.emgStats.data['Voltage.EMG7_Rectify_Env', 'Left']["mean"])
         # plt.show()
         # import ipdb; ipdb.set_trace()
+
+    def test_Coactivation_unithan(self):
+
+        DATA_PATH = "C:\\Users\\fleboeuf\\Documents\\DATA\\pyCGM2-Data-Tests\\EMG\\Normalisation\\Mvc\\"
+
+        emgManager = emg.loadEmg(DATA_PATH)
+        EMG_LABELS = emgManager.getChannels()
+
+
+
+        # trials
+        emg.processEMG(DATA_PATH, ["trial 01.c3d","trial 02.c3d","trial 03.c3d"], emgManager.getChannels(),fileSuffix ="filtered" )
+        emgprocessFiles  = [it[0:it.rfind(".")]+"_filtered.c3d" for it in ["trial 01.c3d","trial 02.c3d","trial 03.c3d"]]
+
+        emgAnalysisInstance = analysis.makeAnalysis(DATA_PATH,
+                            emgprocessFiles,
+                            type="unknow",
+                            kinematicLabelsDict=None,
+                            kineticLabelsDict=None,
+                            emgChannels = EMG_LABELS
+                            )
+
+        emg.normalizedEMG(DATA_PATH,emgAnalysisInstance,method="MeanMax", mvcSettings=None,fromOtherAnalysis=None)
+
+
+        cap = coactivation.UnithanCoActivationProcedure()
+        caf = emgFilters.EmgCoActivationFilter(emgAnalysisInstance,"Left")
+        caf.setEMG1("Voltage.EMG1")
+        caf.setEMG2("Voltage.EMG2")
+        caf.setCoactivationMethod(cap)
+        caf.run()
+
+
+
+    def test_Coactivation_falconer(self):
+
+        DATA_PATH = "C:\\Users\\fleboeuf\\Documents\\DATA\\pyCGM2-Data-Tests\\EMG\\Normalisation\\Mvc\\"
+
+        emgManager = emg.loadEmg(DATA_PATH)
+        EMG_LABELS = emgManager.getChannels()
+
+
+
+        # trials
+        emg.processEMG(DATA_PATH, ["trial 01.c3d","trial 02.c3d","trial 03.c3d"], emgManager.getChannels(),fileSuffix ="filtered" )
+        emgprocessFiles  = [it[0:it.rfind(".")]+"_filtered.c3d" for it in ["trial 01.c3d","trial 02.c3d","trial 03.c3d"]]
+
+        emgAnalysisInstance = analysis.makeAnalysis(DATA_PATH,
+                            emgprocessFiles,
+                            type="unknow",
+                            kinematicLabelsDict=None,
+                            kineticLabelsDict=None,
+                            emgChannels = EMG_LABELS
+                            )
+
+        emg.normalizedEMG(DATA_PATH,emgAnalysisInstance,method="MeanMax", mvcSettings=None,fromOtherAnalysis=None)
+
+
+        cap = coactivation.FalconerCoActivationProcedure()
+        caf = emgFilters.EmgCoActivationFilter(emgAnalysisInstance,"Left")
+        caf.setEMG1("Voltage.EMG1")
+        caf.setEMG2("Voltage.EMG2")
+        caf.setCoactivationMethod(cap)
+        caf.run()
