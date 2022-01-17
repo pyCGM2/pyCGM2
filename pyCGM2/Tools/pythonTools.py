e91750de727712fd39ef9fa059935b22d0aa6642
fleb <fabien.leboeuf@gmail.com>
2022-01-17 10:47:41 +0100

    pythonTools document

--- pyCGM2/Tools/pythonTools.py
+++ pyCGM2/Tools/pythonTools.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
+#APIDOC: /Low level/Tools
 
 class ordict(dict):
-    """Dictionnaire dont les clés restent dans l'ordre d'insertion"""
+    """ordered dictionnary"""
 
     # ========================================================================
     def __init__(self, *args, **kwargs):
@@ -12,14 +12,14 @@ class ordict(dict):
 
     # ========================================================================
     def __setitem__(self, key, value):
-        """initialise la clé avec la valeur"""
+        """init key"""
         if key not in self._keys:
             self._keys.append(key)
         dict.__setitem__(self, key, value)
 
     # ========================================================================
     def __delitem__(self, key):
-        """supprime la clé key (Except. KeyError si elle n'existe pas)"""
+        """delete a key (Except. KeyErrorif not exist)"""
         try:
            dict.__delitem__(self, key)
            if key in self.keys():
@@ -29,24 +29,23 @@ class ordict(dict):
 
     # ========================================================================
     def clear(self):
-        """remet le dictionnaire à vide"""
+        """clear the dictionnary"""
         self._keys = []
         dict.clear(self)
 
     # ========================================================================
     def keys(self):
-        """retourne la liste des clés dans l'ordre d'insertion"""
+        """get keys"""
         return self._keys
 
     # ========================================================================
     def items(self):
-        """retourne la liste des couples (clé, valeur) dans l'ordre d'insertion"""
+        """return items (key,value)"""
         return [(key, self[key]) for key in self._keys]
 
     # ========================================================================
     def pop(self, key, value=None):
-        """retire la clé key et retourne sa valeur
-           si la clé n'existe pas, retourne la valeur par defaut 'value'
+        """remove a key and return its value
         """
         try:
             val = dict.pop(self, key, value)
@@ -58,9 +57,7 @@ class ordict(dict):
 
     # ========================================================================
     def popitem(self):
-        """retire et retourne l'une des clés avec sa valeur
-           si le dico est vide, déclenche une exception
-        """
+        """remove a key an return the key an its value  """
         try:
             key, val = dict.popitem(self)
             self._keys.remove(key)
@@ -70,37 +67,36 @@ class ordict(dict):
 
     # ========================================================================
     def values(self):
-        """retourne la liste des valeurs dans l'ordre d'insertion des clés"""
+        """return values"""
         return [self[key] for key in self._keys]
 
     # ========================================================================
     def __iter__(self):
-        """itérateur renvoyant les clés dans l'ordre d'insertion"""
+        """iterator"""
         for key in self._keys:
             yield key
 
     # ========================================================================
     def iteritems(self):
-        """itérateur renvoyant les tuples (clés, valeur) dans l'ordre d'insertion"""
+        """iterator returning  tuples (key, value)"""
         for key in self._keys:
             yield (key, self[key])
 
     # ========================================================================
     def iterkeys(self):
-        """itérateur renvoyant les clés dans leur ordre d'insertion"""
+        """iterator returning  key """
         for key in self._keys:
             yield key
 
     # ========================================================================
     def itervalues(self):
-        """itérateur renvoyant les valeurs dans l'ordre d'insertion des clés"""
+        """iterator returning value"""
         for key in self._keys:
             yield self[key]
 
     # ========================================================================
     def copy(self):
-        """retourne la copie superficielle du dictionnaire
-           en concervant dans l'ordre des clés
+        """copy dict
         """
         d = ordict()
         for key in self._keys:
@@ -109,19 +105,18 @@ class ordict(dict):
 
     # ========================================================================
     def __repr__(self):
-        """retourne une chaine représentant le dictionnaire ordonné"""
+        """set strings representative of the dictionnary"""
         lst = ['%r: %r' % (key, self[key]) for key in self._keys]
         return type(self).__name__ + '{' + ', '.join(lst) + '}'
 
     # ========================================================================
     def __str__(self):
-        """retourne une chaine représentant le dictionnaire ordonné"""
+        """return strings representative of the dictionnary"""
         return self.__repr__()
 
     # ========================================================================
     def setdefault(self, key, value=None):
-        """si la clé n'existe pas, la crée avec la valeur value
-           dans tous les cas, retourne la valeur de la clé
+        """set a default item
         """
         if key not in self._keys:
             self.__setitem__(key, value)
@@ -129,11 +124,7 @@ class ordict(dict):
 
     # ========================================================================
     def update(self, *args, **kwargs):
-        """ajoute les arguments au dictionnaire dans le bon ordre si possible
-           *args est un dictionnaire (dict ou ordict) ou une liste (clé,valeur)
-           **kwargs est donné comme un tuple clé=valeur, présentée comme un dict
-                            c'est à dire que l'ordre d'appel n'est pas respecté
-        """
+        """update the dictionnary"""
         if len(args)>0:
             args = args[0]
             if isinstance(args, (dict, ordict)):
@@ -144,15 +135,3 @@ class ordict(dict):
                     self.__setitem__(key, value)
         for key in kwargs:
             self.__setitem__(key, kwargs[key])
-
-    # ========================================================================
-    def sort(self, cmp=cmp, key=lambda e: e, reverse=False):
-        """trie les clés sur place (l'ordre initial d'insertion est perdu!)"""
-        self._keys.sort(cmp, key, reverse)
-
-    # ========================================================================
-    def sorted(self, cmp=cmp, key=lambda e: e, reverse=False):
-        """retourne un nouveau dictionnaire ordonné avec les clés triées"""
-        d = self.copy()
-        d._keys.sort(cmp, key, reverse)
-        return d
