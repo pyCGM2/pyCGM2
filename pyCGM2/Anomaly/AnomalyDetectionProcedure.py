2c856ee89058519f22bb1e1e462e8a04c05d8093
fleb <fabien.leboeuf@gmail.com>
2022-05-04 15:37:27 +0200

    no message

--- pyCGM2/Anomaly/AnomalyDetectionProcedure.py
+++ pyCGM2/Anomaly/AnomalyDetectionProcedure.py
@@ -22,8 +22,10 @@ try:
     from pyCGM2 import btk
 except:
     LOGGER.logger.info("[pyCGM2] pyCGM2-embedded btk not imported")
-    import btk
-
+    try:
+        import btk
+    except:
+        LOGGER.logger.error("[pyCGM2] btk not found on your system. install it for working with the API")
 
 class AbstractDetectionProcedure(object):
     """abstract marker detector procedure """
