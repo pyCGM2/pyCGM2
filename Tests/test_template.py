470e3b865423c49b0a79f129a8bcdaa8e8d9fe7a
fleb <fabien.leboeuf@gmail.com>
2023-02-10 08:30:06 +0100

    template test

---
+++ Tests/test_template.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# pytest -s --disable-pytest-warnings --log-cli-level=INFO  test_anomalies.py::Test_markerAnomalies::test_anomalies
+
+import pytest
+
+
+class Test_groupTestName:
+    def test_testname(self):
+        pass
+
+
