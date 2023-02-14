0615b0dcb0663ffadb4f02f48d19483c15f2612e
fleb <fabien.leboeuf@gmail.com>
2023-02-14 08:08:41 +0100

    new method to construct ktk.timeseries from IMU

--- pyCGM2/IMU/imu.py
+++ pyCGM2/IMU/imu.py
@@ -5,20 +5,18 @@ import pandas as pd
 from scipy.signal import find_peaks
 from scipy.linalg import norm
 
+from pyCGM2.External.ktk.kineticstoolkit import timeseries
 class Imu(object):
     """
-    A node is a local position of a 3D point in a coordinate system
-
-    Note:
-        Automatically, the suffix "_node" ends the node label
+    the Inertial meaure unit Class
 
     Args:
-       label(str):  desired label of the node
-       desc(str,Optional):  description
+       freq(integer):  frequency
     """
 
     def __init__(self,freq):
         self.m_freq =  freq
+
         self.m_acceleration = dict()
         self.m_gyro = dict()
         self.m_mag = dict()
@@ -27,12 +25,14 @@ class Imu(object):
 
         self.m_properties = dict()
 
-
     def setAcceleration(self,axis,values):
         self.m_acceleration[axis] = values
 
     def setGyro(self,axis,values):
         self.m_gyro[axis] = values
+
+    def setMagnetometer(self,axis,values):
+        self.m_mag[axis] = values
 
     def downsample(self,freq=400):
 
@@ -68,6 +68,22 @@ class Imu(object):
                 "Gyro.Z": self.m_gyro["Z"]}
 
         self.dataframe = pd.DataFrame(data)
+
+
+    def constructTimeseries(self):
+        """construct a kinetictoolkit timeseries
+        """
+        frames = np.arange(0, self.m_acceleration["X"].shape[0])
+
+        self.m_timeseries = timeseries.TimeSeries()
+        self.m_timeseries.time = frames*1/self.m_freq
+        self.m_timeseries.data["Accel.X"] = self.m_acceleration["X"]
+        self.m_timeseries.data["Accel.Y"] = self.m_acceleration["Y"]
+        self.m_timeseries.data["Accel.Z"] = self.m_acceleration["Z"]
+        self.m_timeseries.data["Gyro.X"] = self.m_gyro["X"]
+        self.m_timeseries.data["Gyro.Y"] = self.m_gyro["Y"]
+        self.m_timeseries.data["Gyro.Z"] = self.m_gyro["Z"]
+
 
     def getAccelerationPeaks(self, threshold=12, distance=50, plot= True):
 
