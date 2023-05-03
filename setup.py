c4c5f521a91f5a6fcd00f076279f60cd0b0b136e
fleb <fabien.leboeuf@gmail.com>
2023-05-03 15:48:44 +0200

    version update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if developMode:
 if sys.maxsize < 2**32:
     raise Exception ("32-bit python version detected. PyCGM2-python3 requires a 64 bits python version")
 
-VERSION ="4.3-rc1"
+VERSION ="4.3rc2"
 
 # just get one of the site-package and install there (it can be dist-package)
 SITE_PACKAGE_PATH = site.getsitepackages()[0] + "\\"
