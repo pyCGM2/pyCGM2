8a8af3c89e29dc6be80d7ffe16625af9ebf928cf
fleb <fabien.leboeuf@gmail.com>
2023-05-04 09:47:42 +0200

    add log

--- pyCGM2/Apps/Commands/initSettingsCmd.py
+++ pyCGM2/Apps/Commands/initSettingsCmd.py
@@ -25,28 +25,40 @@ def main(args=None):
         if args.model == "CGM1" or args.model == "CGM1.0":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER
                             + "CGM1-pyCGM2.settings", os.getcwd()+"\\"+"CGM1-pyCGM2.settings")
-            
+            LOGGER.logger.info("[pyCGM2] file [CGM1-pyCGM2.settings] copied in your data folder")
         if args.model == "CGM1.1":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM1_1-pyCGM2.settings",
                             os.getcwd()+"\\"+"CGM1_1-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM1_1-pyCGM2.settings] copied in your data folder")
         elif args.model == "CGM2.1":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM2_1-pyCGM2.settings",
                             os.getcwd()+"\\"+"CGM2_1-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM2_1-pyCGM2.settings] copied in your data folder")
             # os.startfile(os.getcwd()+"\\"+"CGM2_1-pyCGM2.settings")
         elif args.model == "CGM2.2":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM2_2-pyCGM2.settings",
                             os.getcwd()+"\\"+"CGM2_2-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM2_2-pyCGM2.settings] copied in your data folder")
         elif args.model == "CGM2.3":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM2_3-pyCGM2.settings",
                             os.getcwd()+"\\"+"CGM2_3-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM2_3-pyCGM2.settings] copied in your data folder")
 
         elif args.model == "CGM2.4":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM2_4-pyCGM2.settings",
                             os.getcwd()+"\\"+"CGM2_4-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM2_4-pyCGM2.settings] copied in your data folder")
 
         elif args.model == "CGM2.5":
             files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM2_5-pyCGM2.settings",
                             os.getcwd()+"\\"+"CGM2_5-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM2_5-pyCGM2.settings] copied in your data folder")
+
+        elif args.model == "CGM2.6":
+            files.copyPaste(pyCGM2.PYCGM2_SETTINGS_FOLDER+"CGM2_5-pyCGM2.settings",
+                            os.getcwd()+"\\"+"CGM2_5-pyCGM2.settings")
+            LOGGER.logger.info("[pyCGM2] file [CGM2_5-pyCGM2.settings] copied in your data folder")
+
         else:
             LOGGER.logger.error("[pyCGM2] model version not know (CGM1, CGM1.1 ... CGM2.5)")
             raise
