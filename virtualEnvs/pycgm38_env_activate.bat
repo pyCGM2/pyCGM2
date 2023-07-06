c5d61d137393ad7c8daf9c05b3981dd291e58d24
fleb <fabien.leboeuf@gmail.com>
2023-07-06 11:57:00 +0200

    NEW - activate bat file loadable from nexus

---
+++ virtualEnvs/pycgm38_env_activate.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Set the path to your Miniconda installation directory
+set "CONDA_PATH=C:\Users\fleboeuf\Miniconda3"
+
+rem Set the name of your virtual environment
+set "ENV_NAME=pycgm38"
+
+call "%CONDA_PATH%\Scripts\activate.bat" %ENV_NAME%
