2f52420e691c2425b3ccda9cb402f892d2e94039
fleb <fabien.leboeuf@gmail.com>
2024-02-19 12:22:19 +0100

    test Nexus-pycgm2 updated

--- Tests/test_NexusCgm.py
+++ Tests/test_NexusCgm.py
@@ -6,6 +6,7 @@ import pyCGM2; LOGGER = pyCGM2.LOGGER
 import pytest
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 from viconnexusapi import ViconNexus
 
@@ -24,12 +25,12 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM1\\pyCGM2_FullBody_CGM1_KADmed\\"
             filenameNoExt = "Static"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM1_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM1.0 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "Capture 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM1_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM1.0 Fitting")
             NEXUS.SaveTrial(30)
 
     class Test_CGM11:
@@ -39,12 +40,12 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM1.1\\pyCGM2_FullBody_CGM1_medial\\"
             filenameNoExt = "FullBody CGM2 data Cal 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM11_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM1.1 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "Capture 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM11_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM1.1 Fitting")
             NEXUS.SaveTrial(30)
 
     class Test_CGM21:
@@ -53,12 +54,12 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM2.1\\pyCGM2_FullBody_CGM21\\"
             filenameNoExt = "FullBody CGM2 data Cal 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM21_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.1 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "Capture 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM21_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.1 Fitting")
             NEXUS.SaveTrial(30)
 
     class Test_CGM22:
@@ -67,12 +68,12 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM2.2\\pyCGM2_FullBody_CGM22\\"
             filenameNoExt = "FullBody CGM2 data Cal 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM22_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.2 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "Capture 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM22_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.2 Fitting")
             NEXUS.SaveTrial(30)
 
     class Test_CGM23:
@@ -81,13 +82,16 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM2.3\\pyCGM2_FullBody_CGM23\\"
             filenameNoExt = "FullBody CGM2 data Cal 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM23_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.3 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "Capture 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM23_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.3 Fitting")
             NEXUS.SaveTrial(30)
+
+
+
 
     class Test_CGM24:
         def test_fullBody(self):
@@ -95,12 +99,12 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM2.4\\pyCGM2_FullBody_CGM24\\"
             filenameNoExt = "FullBody CGM2 data Cal 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM24_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.4 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "Capture 01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM24_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.4 Fitting")
             NEXUS.SaveTrial(30)
 
     class Test_CGM25:
@@ -109,10 +113,52 @@ else :
             DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM2.5\\pyCGM2_FullBody_CGM24\\"
             filenameNoExt = "CGM2_Static_01"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM25_Calibration.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.5 Calibration")
             NEXUS.SaveTrial(30)
 
             filenameNoExt = "CGM2_Walk_02"
             NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
-            os.system("Nexus_CGM25_Fitting.exe")
+            os.system("pyCGM2.exe NEXUS CGM2.5 Fitting")
+            NEXUS.SaveTrial(30)
+
+    class Test_plots:
+
+        def test_stp(self):
+
+            DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM23-Patient\\Session 1\\"
+
+            filenameNoExt = "20240214-EC-PONC-F-NNNN04"
+            NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+            os.system("pyCGM2.exe NEXUS Plots STP")
+            NEXUS.SaveTrial(30)
+
+
+        def test_kinematics(self):
+
+            DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM23-Patient\\Session 1\\"
+
+            filenameNoExt = "20240214-EC-PONC-F-NNNN04"
+            NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+            os.system("pyCGM2.exe NEXUS Plots Kinematics Normalized")
+            os.system("pyCGM2.exe NEXUS Plots Kinematics Temporal")
+            os.system("pyCGM2.exe NEXUS Plots Kinematics MAP")
+            NEXUS.SaveTrial(30)
+
+        def test_kinetics(self):
+
+            DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM23-Patient\\Session 1\\"
+
+            filenameNoExt = "20240214-EC-PONC-F-NNNN04"
+            NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+            os.system("pyCGM2.exe NEXUS Plots Kinetics Normalized")
+            #os.system("pyCGM2.exe NEXUS Plots Kinetics Temporal")
+            NEXUS.SaveTrial(30)
+
+        def test_reaction(self):
+
+            DATA_PATH =  pyCGM2.TEST_DATA_PATH+"Nexus\\CGM23-Patient\\Session 1\\"
+
+            filenameNoExt = "20240214-EC-PONC-F-NNNN04"
+            NEXUS.OpenTrial( str(DATA_PATH+filenameNoExt), 30 )
+            os.system("pyCGM2.exe NEXUS Plots Reaction Temporal")
             NEXUS.SaveTrial(30)
